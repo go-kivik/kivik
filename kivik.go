c6c5646a10efe67236ac7193b1b1fe7aa1eed628
Jonathan Hall <flimzy@flimzy.com>
2017-02-25 19:45:37 +0100

    Add auth abstraction layer
    Only supports HTTP Basic Auth for CouchDB for now, but should be easily expandible.

--- kivik.go
+++ kivik.go
@@ -101,3 +101,14 @@ func (c *Client) CreateDB(dbName string) error {
 func (c *Client) DestroyDB(dbName string) error {
 	return c.driverClient.DestroyDB(dbName)
 }
+
+// SetAuth overrides any existing authentication method with that passed. The
+// argument passed must be of a type understood by the underlying driver. Not
+// all drivers implement authentication. If the type passed is not understood
+// by the driver, an error will be returned.
+func (c *Client) SetAuth(a interface{}) error {
+	if auth, ok := c.driverClient.(driver.Authenticator); ok {
+		return auth.SetAuth(a)
+	}
+	return NotImplemented
+}

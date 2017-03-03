8ecfe228621eb0d90a6c58764cb4440943c5dfc3
Jonathan Hall <flimzy@flimzy.com>
2017-03-03 16:32:45 +0100

    Some reorganization and DRY for CreateDB() tests

--- test/createdb.go
+++ test/createdb.go
@@ -17,35 +17,30 @@ func init() {
 // CreateDB tests database creation.
 func CreateDB(clients *Clients, suite string, t *testing.T) {
 	t.Run("Admin", func(t *testing.T) {
-		testCreateDB(clients.Admin, t)
+		t.Parallel()
+		testDB := testDBName()
+		defer clients.Admin.DestroyDB(testDB)
+		testCreateDB(clients.Admin, testDB, 0, t)
 	})
 	if clients.NoAuth == nil {
 		return
 	}
 	t.Run("NoAuth", func(t *testing.T) {
-		testCreateDBUnauthorized(clients.NoAuth, t)
+		t.Parallel()
+		testDB := testDBName()
+		defer clients.NoAuth.DestroyDB(testDB) // Just in case we succeed
+		testCreateDB(clients.NoAuth, testDB, http.StatusUnauthorized, t)
 	})
 }
 
-func testCreateDBUnauthorized(client *kivik.Client, t *testing.T) {
-	testDB := testDBName()
-	defer client.DestroyDB(testDB) // Just in case we succeed
-	err := client.CreateDB(testDB)
+func testCreateDB(client *kivik.Client, dbName string, status int, t *testing.T) {
+	err := client.CreateDB(dbName)
 	switch errors.StatusCode(err) {
-	case 0:
-		t.Errorf("CreateDB: Should fail for unauthenticated session")
-	case http.StatusUnauthorized:
+	case status:
 		// Expected
+	case 0:
+		t.Errorf("Expected failure creating database '%s' %d/%s", dbName, status, http.StatusText(status))
 	default:
-		t.Errorf("CreateDB: Expected 401/Unauthorized, Got: %s", err)
-	}
-}
-
-func testCreateDB(client *kivik.Client, t *testing.T) {
-	testDB := testDBName()
-	defer client.DestroyDB(testDB)
-	err := client.CreateDB(testDB)
-	if err != nil {
-		t.Errorf("Failed to create database '%s': %s", testDB, err)
+		t.Errorf("Failed to create database '%s': %s", dbName, err)
 	}
 }

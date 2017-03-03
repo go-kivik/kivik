1714dfabb6506a75fb08e4873ab9c5eb5bb3c9d8
Jonathan Hall <flimzy@flimzy.com>
2017-03-03 15:48:55 +0100

    Add Unauthorized tests for db/_all_docs

--- test/db_alldocs.go
+++ test/db_alldocs.go
@@ -1,6 +1,12 @@
 package test
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+
+	"github.com/flimzy/kivik"
+	"github.com/flimzy/kivik/errors"
+)
 
 func init() {
 	for _, suite := range []string{SuiteCouch16, SuiteCouch20, SuiteCloudant} {
@@ -13,11 +19,27 @@ func init() {
 }
 
 // AllDocsCouch tests the /{db}/_all_docs endpoint for CouchDB-like backends.
-func AllDocsCouch(clients *Clients, _ string, t *testing.T) {
-	client := clients.Admin
+func AllDocsCouch(clients *Clients, suite string, t *testing.T) {
+	t.Run("Admin", func(t *testing.T) {
+		testAllDocsCouch(clients.Admin, t)
+	})
+	if clients.NoAuth == nil {
+		return
+	}
+	expectedStatus := http.StatusForbidden
+	if suite == SuiteCloudant {
+		expectedStatus = http.StatusUnauthorized
+	}
+	t.Run("NoAuth", func(t *testing.T) {
+		testAllDocsCouchFailure(clients.NoAuth, expectedStatus, t)
+	})
+}
+
+func testAllDocsCouch(client *kivik.Client, t *testing.T) {
 	db, err := client.DB("_replicator")
 	if err != nil {
 		t.Errorf("Failed to connect to database: %s", err)
+		return
 	}
 	docs := []interface{}{}
 	offset, total, _, err := db.AllDocs(&docs, nil)
@@ -30,5 +52,22 @@ func AllDocsCouch(clients *Clients, _ string, t *testing.T) {
 	}
 	if total < 1 {
 		t.Errorf("Expected total >= 1, got %d", total)
+	}
+}
+
+func testAllDocsCouchFailure(client *kivik.Client, expectedStatus int, t *testing.T) {
+	db, err := client.DB("_replicator")
+	if err != nil {
+		t.Errorf("Failed to connect to database: %s", err)
+		return
+	}
+	_, _, _, err = db.AllDocs(&struct{}{}, nil)
+	switch errors.StatusCode(err) {
+	case 0:
+		t.Errorf("Expected an error")
+	case expectedStatus:
+		// expected
+	default:
+		t.Errorf("Expected %s, got %s", http.StatusText(expectedStatus), err)
 	}
 }

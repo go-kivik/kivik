51ff6abd33fdc2965ab583658a2686ec3d98aeca
Jonathan Hall <flimzy@flimzy.com>
2017-03-04 10:33:35 +0100

    Add Get() tests

--- test/db_get.go
+++ test/db_get.go
@@ -1,16 +1,48 @@
 package test
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func init() {
-	// for _, suite := range AllSuites {
-	// 	RegisterTest(suite, "Get", true, Get)
-	// }
+	for _, suite := range []string{SuiteCouch16, SuiteCouch20, SuiteCloudant} {
+		RegisterTest(suite, "Get", true, Get)
+	}
 }
 
 // Get tests fetching of documents.
 func Get(clients *Clients, suite string, t *testing.T) {
+	admin := clients.Admin
+	// noAuth := clients.NoAuth
 	testDB := testDBName()
-	defer clients.Admin.DestroyDB(testDB)
-
+	defer admin.DestroyDB(testDB)
+	if err := admin.CreateDB(testDB); err != nil {
+		t.Errorf("Failed to create database '%s': %s", testDB, err)
+		return
+	}
+	adminDB, err := admin.DB(testDB)
+	if err != nil {
+		t.Errorf("Failed to connect to database: %s", err)
+		return
+	}
+	doc := &testDoc{
+		ID:   "bob",
+		Name: "Robert",
+		Age:  32,
+	}
+	rev, err := adminDB.Put(doc.ID, doc)
+	if err != nil {
+		t.Errorf("Failed to create document: %s", err)
+		return
+	}
+	doc.Rev = rev
+	recvDoc := &testDoc{}
+	if err = adminDB.Get(doc.ID, recvDoc, nil); err != nil {
+		t.Errorf("Failed to fetch document: %s", err)
+		return
+	}
+	if !reflect.DeepEqual(doc, recvDoc) {
+		t.Errorf("Retrieved document does not match")
+	}
 }

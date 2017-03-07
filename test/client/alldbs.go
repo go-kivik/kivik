332d31c7f5c59d656279bcb576f20546ed19cff9
Jonathan Hall <flimzy@flimzy.com>
2017-03-07 11:07:12 +0100

    Run AllDBs tests in parallel

--- test/client/alldbs.go
+++ test/client/alldbs.go
@@ -33,15 +33,18 @@ func testAllDBsRW(ctx *kt.Context) {
 		return
 	}
 	expected := append(ctx.StringSlice("expected"), dbName)
-	ctx.RunAdmin(func(ctx *kt.Context) {
-		testAllDBs(ctx, ctx.Admin, expected)
-	})
-	ctx.RunNoAuth(func(ctx *kt.Context) {
-		testAllDBs(ctx, ctx.NoAuth, expected)
+	ctx.Run("group", func(ctx *kt.Context) {
+		ctx.RunAdmin(func(ctx *kt.Context) {
+			testAllDBs(ctx, ctx.Admin, expected)
+		})
+		ctx.RunNoAuth(func(ctx *kt.Context) {
+			testAllDBs(ctx, ctx.NoAuth, expected)
+		})
 	})
 }
 
 func testAllDBs(ctx *kt.Context, client *kivik.Client, expected []string) {
+	ctx.Parallel()
 	allDBs, err := client.AllDBs()
 	if !ctx.IsExpectedSuccess(err) {
 		return

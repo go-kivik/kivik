f1436a64d82bb60ee146112c1ea222d158f50bcc
Jonathan Hall <flimzy@flimzy.com>
2017-03-09 18:44:22 +0100

    Simplify CouchDB auth usage

--- driver/couchdb/auth.go
+++ driver/couchdb/auth.go
@@ -7,35 +7,12 @@ import (
 )
 
 type authenticator interface {
-	authenticate(*client) error
-}
-
-// BasicAuth provides basic HTTP Authentication services.
-type BasicAuth struct {
-	*chttp.BasicAuth
-}
-
-var _ authenticator = &BasicAuth{}
-
-func (a *BasicAuth) authenticate(c *client) error {
-	return c.Auth(a)
-}
-
-// CookieAuth provides CouchDB Cookie auth services as described at
-// http://docs.couchdb.org/en/2.0.0/api/server/authn.html#cookie-authentication
-type CookieAuth struct {
-	*chttp.CookieAuth
-}
-
-var _ authenticator = &CookieAuth{}
-
-func (a *CookieAuth) authenticate(c *client) error {
-	return c.Auth(a)
+	Authenticate(*chttp.Client) error
 }
 
 func (c *client) Authenticate(a interface{}) error {
 	if auth, ok := a.(authenticator); ok {
-		return auth.authenticate(c)
+		return auth.Authenticate(c.Client)
 	}
 	return errors.New("invalid authenticator")
 }

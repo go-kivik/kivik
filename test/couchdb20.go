20af78b9c587c2f90f3367cde7560868da25a13b
Jonathan Hall <flimzy@flimzy.com>
2017-03-13 21:14:44 +0100

    Disable _config tests in CDB2.0 for now

--- test/couchdb20.go
+++ test/couchdb20.go
@@ -9,6 +9,7 @@ func init() {
 	RegisterSuite(SuiteCouch20, kt.SuiteConfig{
 		"AllDBs.expected": []string{"_global_changes", "_replicator", "_users"},
 
+		"Config.skip": true, // FIXME: CouchDB config has moved
 		"Config/Admin/GetAll.expected_sections": []string{"admins", "attachments", "compaction_daemon", "cors", "couch_httpd_auth",
 			"couch_httpd_oauth", "couchdb", "daemons", "database_compaction", "httpd", "httpd_db_handlers", "httpd_design_handlers",
 			"httpd_global_handlers", "log", "query_server_config", "query_servers", "replicator", "ssl", "stats", "uuids", "vendor",

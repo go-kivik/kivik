a2033e8c3b3bac000ea481b6b9604ee5cabc3907
Jonathan Hall <flimzy@flimzy.com>
2017-03-13 14:40:04 +0100

    Configure tests to run against Dockerized CouchDB 1.6.1 & 2.0.0
    But only in Linux, as Docker isn't supported in OSX.

--- travis/install.sh
+++ travis/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euC
+set -o xtrace
 
 # Install generic dependencies, needed for all builds
 go get github.com/pborman/uuid \
@@ -7,6 +8,35 @@ go get github.com/pborman/uuid \
     golang.org/x/net/publicsuffix \
     github.com/flimzy/diff \
     golang.org/x/crypto/pbkdf2
+
+function wait_for_server {
+    printf "Waiting for $1"
+    n=0
+    until [ $n -gt 5 ]; do
+        curl --output /dev/null --silent --head --fail $1 && break
+        printf '.'
+        n=$[$n+1]
+        sleep 1
+    done
+    printf "ready!\n"
+}
+
+function setup_docker {
+    if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+        return
+    fi
+    docker pull couchdb:1.6.1
+    docker run -d -p 6000:5984 --name couchdb16 couchdb:1.6.1
+    wait_for_server http://localhost:6000/
+    curl -X PUT http://localhost:6000/_config/admins/admin -d '"abc123"'
+    docker pull couchdb:latest
+    docker run -d -p 6001:5984 --name couchdb20 couchdb:latest
+    wait_for_server http://localhost:6001/
+    curl -X PUT http://localhost:6001/_users
+    curl -X PUT http://localhost:6001/_replicator
+    curl -X PUT http://localhost:6001/_global_changes
+    curl -X PUT http://localhost:6001/_config/admins/admin -d '"abc123"'
+}
 
 case "$1" in
     "standard")
@@ -20,6 +50,7 @@ case "$1" in
             github.com/justinas/alice
         go get -u github.com/jteeuwen/go-bindata/...
         go generate ./...
+        setup_docker
     ;;
     "gopherjs")
         if [ "$TRAVIS_OS_NAME" == "linux" ]; then
@@ -32,8 +63,9 @@ case "$1" in
         # Install Go deps only needed by PouchDB driver
         go get github.com/imdario/mergo
         # Then install GopherJS and related dependencies
-        go get -u github.com/gopherjs/gopherjs; \
-        go get -u -d -tags=js github.com/gopherjs/jsbuiltin; \
+        go get -u github.com/gopherjs/gopherjs
+        go get -u -d -tags=js github.com/gopherjs/jsbuiltin
+        setup_docker
     ;;
     "linter")
         go get -u gopkg.in/alecthomas/gometalinter.v1
--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euC
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    # We don't have docker in OSX, so skip these tests
+    unset KIVIK_TEST_DSN_COUCH16
+    unset KIVIK_TEST_DSN_COUCH20
+fi
+
 case "$1" in
     "standard")
         go test $(go list ./... | grep -v pouchdb)

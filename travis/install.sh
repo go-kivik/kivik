a29fc5442a8173482c41726da64a08fb7dc4a6c4
Jonathan Hall <flimzy@flimzy.com>
2017-03-13 15:58:42 +0100

    turn off delayed commits to reduce race conditions in tests.

--- travis/install.sh
+++ travis/install.sh
@@ -29,11 +29,13 @@ function setup_docker {
     docker run -d -p 6000:5984 --name couchdb16 couchdb:1.6.1
     wait_for_server http://localhost:6000/
     curl -X PUT http://localhost:6000/_config/log/file -d '"/tmp/log"'
+    curl -X PUT http://localhost:6000/_config/couchdb/delayed_commits -d '"false"'
     curl -X PUT http://localhost:6000/_config/admins/admin -d '"abc123"'
     docker pull couchdb:latest
     docker run -d -p 6001:5984 --name couchdb20 couchdb:latest
     wait_for_server http://localhost:6001/
     curl -X PUT http://localhost:6001/_config/log/file -d '"/tmp/log"'
+    curl -X PUT http://localhost:6001/_config/couchdb/delayed_commits -d '"false"'
     curl -X PUT http://localhost:6001/_users
     curl -X PUT http://localhost:6001/_replicator
     curl -X PUT http://localhost:6001/_global_changes

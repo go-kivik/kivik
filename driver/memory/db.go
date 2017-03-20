386ee7dc5addea19aad72a4b8d92473d5dd1decd
Jonathan Hall <flimzy@flimzy.com>
2017-03-20 12:26:25 +0100

    Unimplement AllDocs for mem driver for now

--- driver/memory/db.go
+++ driver/memory/db.go
@@ -28,33 +28,8 @@ func (d *db) SetOption(_ string, _ interface{}) error {
 }
 
 func (d *db) AllDocsContext(ctx context.Context, opts map[string]interface{}) (driver.Rows, error) {
+	// FIXME: Unimplemented
 	return nil, nil
-	// if exists, _ := d.client.DBExistsContext(ctx, d.dbName); !exists {
-	// 	return 0, 0, "", errors.Status(kivik.StatusNotFound, "database not found")
-	// }
-	// db := d.getDB()
-	// db.mutex.RLock()
-	// defer db.mutex.RUnlock()
-	// index := make([]indexDoc, 0, len(db.docs))
-	// for id, doc := range db.docs {
-	// 	index = append(index, indexDoc{
-	// 		ID:  id,
-	// 		Key: id,
-	// 		Value: indexDocValue{
-	// 			Rev: doc.revs[len(doc.revs)-1].Rev,
-	// 		},
-	// 	})
-	// }
-	//
-	// body, err := json.Marshal(ouchdb.AllDocsResponse{
-	// 	Offset:    0,
-	// 	TotalRows: len(index),
-	// 	Rows:      index,
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// return ouchdb.AllDocs(bytes.NewReader(body), docs)
 }
 
 func (d *db) GetContext(_ context.Context, docID string, doc interface{}, opts map[string]interface{}) error {

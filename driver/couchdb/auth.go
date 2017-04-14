f93324335ba028ce5c2686d3c7acbfccc9cacbc9
Piotr Zduniak <piotr@zduniak.net>
2017-04-14 15:12:46 +0200

    Fixed CouchDB authentication interface

--- driver/couchdb/auth.go
+++ driver/couchdb/auth.go
@@ -1,18 +1,15 @@
 package couchdb
 
 import (
+	"context"
 	"errors"
 
 	"github.com/flimzy/kivik/driver/couchdb/chttp"
 )
 
-type authenticator interface {
-	Authenticate(*chttp.Client) error
-}
-
-func (c *client) Authenticate(a interface{}) error {
-	if auth, ok := a.(authenticator); ok {
-		return auth.Authenticate(c.Client)
+func (c *client) AuthenticateContext(ctx context.Context, a interface{}) error {
+	if auth, ok := a.(chttp.Authenticator); ok {
+		return auth.Authenticate(ctx, c.Client)
 	}
 	return errors.New("invalid authenticator")
 }

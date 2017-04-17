a35ca7953a7482e3f4dd09c02950b136f51377f0
Jonathan Hall <flimzy@flimzy.com>
2017-04-17 13:43:11 +0200

    List TestImports, for more accurate test coverage stats

--- travis/test.sh
+++ travis/test.sh
@@ -35,7 +35,7 @@ case "$1" in
 
         for d in $TEST_PKGS; do
             go test -i $d
-            DEPS=$((go list -f $'{{range $f := .Imports}}{{$f}}\n{{end}}' $d && echo $d) | grep -v /vendor/ | grep -v /pouchdb | grep -v /kivik/test | grep ^github.com/flimzy/kivik | tr '\n' ' ')
+            DEPS=$((go list -f $'{{range $f := .TestImports}}{{$f}}\n{{end}}{{range $f := .Imports}}{{$f}}\n{{end}}' $d && echo $d) | sort -u | grep -v /vendor/ | grep -v /pouchdb | grep -v /kivik/test | grep ^github.com/flimzy/kivik | tr '\n' ' ')
             go test -coverprofile=profile.out -covermode=set -coverpkg=$(join_list $DEPS) $d
             if [ -f profile.out ]; then
                 cat profile.out >> coverage.txt

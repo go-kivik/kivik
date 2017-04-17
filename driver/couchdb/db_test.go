71136af9fff8c97ebf3926ecc3a2a6348bf3d909
Jonathan Hall <flimzy@flimzy.com>
2017-04-17 13:12:50 +0200

    Add tests for optionsToParams()

--- driver/couchdb/db_test.go
+++ driver/couchdb/db_test.go
@@ -3,8 +3,10 @@ package couchdb
 import (
 	"context"
 	"io"
+	"net/url"
 	"testing"
 
+	"github.com/flimzy/diff"
 	"github.com/flimzy/kivik"
 	"github.com/flimzy/kivik/driver"
 )
@@ -45,5 +47,58 @@ func TestDBInfo(t *testing.T) {
 	}
 	if info.Name != "_users" {
 		t.Errorf("Unexpected name %s", info.Name)
+	}
+}
+
+func TestOptionsToParams(t *testing.T) {
+	type otpTest struct {
+		Name     string
+		Input    map[string]interface{}
+		Expected url.Values
+		Error    string
+	}
+	tests := []otpTest{
+		{
+			Name:     "String",
+			Input:    map[string]interface{}{"foo": "bar"},
+			Expected: map[string][]string{"foo": []string{"bar"}},
+		},
+		{
+			Name:     "StringSlice",
+			Input:    map[string]interface{}{"foo": []string{"bar", "baz"}},
+			Expected: map[string][]string{"foo": []string{"bar", "baz"}},
+		},
+		{
+			Name:     "Bool",
+			Input:    map[string]interface{}{"foo": true},
+			Expected: map[string][]string{"foo": []string{"true"}},
+		},
+		{
+			Name:     "Int",
+			Input:    map[string]interface{}{"foo": 123},
+			Expected: map[string][]string{"foo": []string{"123"}},
+		},
+		{
+			Name:  "Error",
+			Input: map[string]interface{}{"foo": []byte("foo")},
+			Error: "cannot convert type []uint8 to []string",
+		},
+	}
+	for _, test := range tests {
+		func(test otpTest) {
+			t.Run(test.Name, func(t *testing.T) {
+				params, err := optionsToParams(test.Input)
+				var msg string
+				if err != nil {
+					msg = err.Error()
+				}
+				if msg != test.Error {
+					t.Errorf("Error\n\tExpected: %s\n\t  Actual: %s\n", test.Error, msg)
+				}
+				if d := diff.Interface(test.Expected, params); d != "" {
+					t.Errorf("Params not as expected:\n%s\n", d)
+				}
+			})
+		}(test)
 	}
 }

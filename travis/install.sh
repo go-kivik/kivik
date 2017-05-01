4b92ebaa4ee87be360e8afba75cae80b2eec2b7c
Jonathan Hall <flimzy@flimzy.com>
2017-05-01 10:41:36 +0200

    Set admin password via environment, and reduce replication timeout for tests

--- travis/install.sh
+++ travis/install.sh
@@ -37,16 +37,15 @@ function setup_docker {
         return
     fi
     docker pull couchdb:1.6.1
-    docker run -d -p 6000:5984 --name couchdb16 couchdb:1.6.1
+    docker run -d -p 6000:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=abc123 --name couchdb16 couchdb:1.6.1
     wait_for_server http://localhost:6000/
-    curl --silent --fail -o /dev/null -X PUT http://localhost:6000/_config/admins/admin -d '"abc123"'
+    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6000/_config/replicator/connection_timeout -d '"10000"'
     docker pull klaemo/couchdb:latest
-    docker run -d -p 6001:5984 --name couchdb20 klaemo/couchdb:latest
+    docker run -d -p 6001:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=abc123 --name couchdb20 klaemo/couchdb:latest
     wait_for_server http://localhost:6001/
-    curl --silent --fail -o /dev/null -X PUT http://localhost:6001/_users
-    curl --silent --fail -o /dev/null -X PUT http://localhost:6001/_replicator
-    curl --silent --fail -o /dev/null -X PUT http://localhost:6001/_global_changes
-    curl --silent --fail -o /dev/null -X PUT http://localhost:6001/_node/nonode@nohost/_config/admins/admin -d '"abc123"'
+    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6001/_users
+    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6001/_replicator
+    curl --silent --fail -o /dev/null -X PUT http://admin:abc123@localhost:6001/_global_changes
 }
 
 case "$1" in

8b277d5d448b8a81dc44f762d79ec85feebcc89f
Jonathan Hall <flimzy@flimzy.com>
2017-05-06 12:19:37 +0200

    Add proxy URL mapping function

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -3,13 +3,19 @@ package proxy
 
 import (
 	"errors"
+	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Proxy is an http.Handler which proxies connections to a CouchDB server.
 type Proxy struct {
 	baseURL *url.URL
+	// path is the url.Path with trailing slash removed
+	path string
 }
+
+var _ http.Handler = &Proxy{}
 
 // New returns a new Proxy instance, which redirects all requests to the
 // specified URL.
@@ -24,5 +30,24 @@ func New(serverURL string) (*Proxy, error) {
 	if parsed.User != nil {
 		return nil, errors.New("proxy URL must not contain auth credentials")
 	}
-	return &Proxy{baseURL: parsed}, nil
+	return &Proxy{
+		baseURL: parsed,
+		path:    strings.TrimSuffix(parsed.Path, "/"),
+	}, nil
+}
+
+// url maps the request's URL to the backend server.
+func (p *Proxy) url(reqURL *url.URL) string {
+	newURL := url.URL{
+		Scheme:   p.baseURL.Scheme,
+		User:     reqURL.User,
+		Host:     p.baseURL.Host,
+		RawQuery: reqURL.RawQuery,
+	}
+	newURL.Path = p.path + "/" + strings.TrimPrefix(reqURL.Path, "/")
+	return newURL.String()
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 }
--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -1,6 +1,9 @@
 package proxy
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
 	type newTest struct {
@@ -31,6 +34,7 @@ func TestNew(t *testing.T) {
 	for _, test := range tests {
 		func(test newTest) {
 			t.Run(test.Name, func(t *testing.T) {
+				t.Parallel()
 				_, err := New(test.URL)
 				var msg string
 				if err != nil {
@@ -38,6 +42,60 @@ func TestNew(t *testing.T) {
 				}
 				if msg != test.Error {
 					t.Errorf("Expected error: %s\n  Actual error: %s", test.Error, msg)
+				}
+			})
+		}(test)
+	}
+}
+
+func TestURL(t *testing.T) {
+	type urlTest struct {
+		Name       string
+		ServerURL  string
+		RequestURL string
+		Expected   string
+	}
+	tests := []urlTest{
+		{
+			Name:       "Root",
+			ServerURL:  "https://foobar.com:9000/foo",
+			RequestURL: "http://foo.com:1234/",
+			Expected:   "https://foobar.com:9000/foo/",
+		},
+		{
+			Name:       "FullURL",
+			ServerURL:  "https://foobar.com:9000/foo/",
+			RequestURL: "http://foo.com:1234/_replicator",
+			Expected:   "https://foobar.com:9000/foo/_replicator",
+		},
+		{
+			Name:       "BasicAuth",
+			ServerURL:  "https://foobar.com:9000/foo/",
+			RequestURL: "http://bob:abc123@foo.com:1234/_replicator",
+			Expected:   "https://bob:abc123@foobar.com:9000/foo/_replicator",
+		},
+		{
+			Name:       "Query",
+			ServerURL:  "https://foobar.com:9000/foo/",
+			RequestURL: "http://bob:abc123@foo.com:1234/_replicator?yesterday=today",
+			Expected:   "https://bob:abc123@foobar.com:9000/foo/_replicator?yesterday=today",
+		},
+	}
+	for _, test := range tests {
+		func(test urlTest) {
+			t.Run(test.Name, func(t *testing.T) {
+				t.Parallel()
+				p, err := New(test.ServerURL)
+				if err != nil {
+					t.Fatalf("Proxy instantiation failed: %s", err)
+				}
+				reqURL, err := url.Parse(test.RequestURL)
+				if err != nil {
+					t.Fatalf("Failed to parse URL: %s", err)
+				}
+				result := p.url(reqURL)
+				if result != test.Expected {
+					t.Errorf("Expected: %s\n  Actual: %s", test.Expected, result)
 				}
 			})
 		}(test)

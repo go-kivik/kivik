7fecf3e3222508e76d7ebbc7ee9660e54b07b89f
Jonathan Hall <flimzy@flimzy.com>
2017-06-04 23:28:30 +0200

    More complete storage tests

--- driver/memory/store_test.go
+++ driver/memory/store_test.go
@@ -3,6 +3,8 @@ package memory
 import (
 	"strings"
 	"testing"
+
+	"github.com/flimzy/diff"
 )
 
 func TestRandStr(t *testing.T) {
@@ -44,4 +46,72 @@ func TestAddLocalRevision(t *testing.T) {
 	if r != "1-0" {
 		t.Errorf("Expected local revision, got %s", r)
 	}
+}
+
+func TestGetRevisionMissing(t *testing.T) {
+	d := &database{
+		docs: make(map[string]*document),
+	}
+	_, found := d.getRevision("foo", "bar")
+	if found {
+		t.Errorf("Should not have found missing revision")
+	}
+}
+
+func TestGetRevisionFound(t *testing.T) {
+	d := &database{
+		docs: make(map[string]*document),
+	}
+	r := d.addRevision(map[string]interface{}{"_id": "foo", "a": 1})
+	_ = d.addRevision(map[string]interface{}{"_id": "foo", "a": 2})
+	result, found := d.getRevision("foo", r)
+	if !found {
+		t.Errorf("Should have found revision")
+	}
+	expected := map[string]interface{}{"_id": "foo", "a": 1, "_rev": r}
+	if d := diff.AsJSON(expected, result.data); d != "" {
+		t.Error(d)
+	}
+}
+
+func TestRev(t *testing.T) {
+	t.Run("Missing", func(t *testing.T) {
+		d := jsondoc{}
+		if d.Rev() != "" {
+			t.Errorf("Rev should be missing, but got %s", d.Rev())
+		}
+	})
+	t.Run("Set", func(t *testing.T) {
+		d := jsondoc{"_rev": "foo"}
+		if d.Rev() != "foo" {
+			t.Errorf("Rev should be foo, but got %s", d.Rev())
+		}
+	})
+	t.Run("NonString", func(t *testing.T) {
+		d := jsondoc{"_rev": true}
+		if d.Rev() != "" {
+			t.Errorf("Rev should be missing, but got %s", d.Rev())
+		}
+	})
+}
+
+func TestID(t *testing.T) {
+	t.Run("Missing", func(t *testing.T) {
+		d := jsondoc{}
+		if d.ID() != "" {
+			t.Errorf("ID should be missing, but got %s", d.ID())
+		}
+	})
+	t.Run("Set", func(t *testing.T) {
+		d := jsondoc{"_id": "foo"}
+		if d.ID() != "foo" {
+			t.Errorf("ID should be foo, but got %s", d.ID())
+		}
+	})
+	t.Run("NonString", func(t *testing.T) {
+		d := jsondoc{"_id": true}
+		if d.ID() != "" {
+			t.Errorf("ID should be missing, but got %s", d.ID())
+		}
+	})
 }

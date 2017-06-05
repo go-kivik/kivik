e792247aee856db55e6c69176d9cfcfe8069abc0
Jonathan Hall <flimzy@flimzy.com>
2017-06-05 16:09:05 +0200

    Add the ability to set security

--- driver/memory/security.go
+++ driver/memory/security.go
@@ -8,24 +8,34 @@ import (
 	"github.com/flimzy/kivik/errors"
 )
 
+func cloneSecurity(in *driver.Security) *driver.Security {
+	return &driver.Security{
+		Admins: driver.Members{
+			Names: in.Admins.Names,
+			Roles: in.Admins.Roles,
+		},
+		Members: driver.Members{
+			Names: in.Members.Names,
+			Roles: in.Members.Roles,
+		},
+	}
+}
+
 func (d *db) Security(_ context.Context) (*driver.Security, error) {
+	d.db.mu.RLock()
+	defer d.db.mu.RUnlock()
 	if d.db.deleted {
 		return nil, errors.Status(kivik.StatusNotFound, "missing")
 	}
-	// deep copy
-	return &driver.Security{
-		Admins: driver.Members{
-			Names: d.db.security.Admins.Names,
-			Roles: d.db.security.Admins.Roles,
-		},
-		Members: driver.Members{
-			Names: d.db.security.Members.Names,
-			Roles: d.db.security.Members.Roles,
-		},
-	}, nil
+	return cloneSecurity(d.db.security), nil
 }
 
-func (d *db) SetSecurity(_ context.Context, _ *driver.Security) error {
-	// FIXME: Unimplemented
-	return notYetImplemented
+func (d *db) SetSecurity(_ context.Context, sec *driver.Security) error {
+	d.db.mu.Lock()
+	defer d.db.mu.Unlock()
+	if d.db.deleted {
+		return errors.Status(kivik.StatusNotFound, "missing")
+	}
+	d.db.security = cloneSecurity(sec)
+	return nil
 }
--- driver/memory/security_test.go
+++ driver/memory/security_test.go
@@ -72,6 +72,7 @@ func TestGetSecurity(t *testing.T) {
 	for _, test := range tests {
 		func(test secTest) {
 			t.Run(test.Name, func(t *testing.T) {
+				t.Parallel()
 				db := test.DB
 				if db == nil {
 					db = setupDB(t, nil)
@@ -86,6 +87,88 @@ func TestGetSecurity(t *testing.T) {
 				}
 				if err != nil {
 					return
+				}
+				if d := diff.AsJSON(test.Expected, sec); d != "" {
+					t.Error(d)
+				}
+			})
+		}(test)
+	}
+}
+
+func TestSetSecurity(t *testing.T) {
+	type setTest struct {
+		Name     string
+		Security *driver.Security
+		Error    string
+		Expected *driver.Security
+		DB       driver.DB
+	}
+	tests := []setTest{
+		{
+			Name:  "DBNotFound",
+			Error: "missing",
+			DB: func() driver.DB {
+				c := setup(t, nil)
+				if err := c.CreateDB(context.Background(), "foo", nil); err != nil {
+					t.Fatal(err)
+				}
+				db, err := c.DB(context.Background(), "foo", nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if e := c.DestroyDB(context.Background(), "foo", nil); e != nil {
+					t.Fatal(e)
+				}
+				return db
+			}(),
+		},
+		{
+			Name: "Valid",
+			Security: &driver.Security{
+				Admins: driver.Members{
+					Names: []string{"foo", "bar", "baz"},
+					Roles: []string{"morons"},
+				},
+				Members: driver.Members{
+					Names: []string{"bob"},
+					Roles: []string{"boring"},
+				},
+			},
+			Expected: &driver.Security{
+				Admins: driver.Members{
+					Names: []string{"foo", "bar", "baz"},
+					Roles: []string{"morons"},
+				},
+				Members: driver.Members{
+					Names: []string{"bob"},
+					Roles: []string{"boring"},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		func(test setTest) {
+			t.Run(test.Name, func(t *testing.T) {
+				t.Parallel()
+				db := test.DB
+				if db == nil {
+					db = setupDB(t, nil)
+				}
+				err := db.SetSecurity(context.Background(), test.Security)
+				var msg string
+				if err != nil {
+					msg = err.Error()
+				}
+				if msg != test.Error {
+					t.Errorf("Unexpected error: %s", msg)
+				}
+				if err != nil {
+					return
+				}
+				sec, err := db.Security(context.Background())
+				if err != nil {
+					t.Fatal(err)
 				}
 				if d := diff.AsJSON(test.Expected, sec); d != "" {
 					t.Error(d)

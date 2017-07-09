5a81c2735b70ce3ae7896fa854fbd90bf746746e
Jonathan Hall <flimzy@flimzy.com>
2017-07-09 16:13:19 +0200

    No longer depend on the memory driver for unit tests

--- serve/couchserver/db_test.go
+++ serve/couchserver/db_test.go
@@ -11,52 +11,110 @@ import (
 	"github.com/flimzy/kivik"
 )
 
+type mockCreator struct {
+	backend
+}
+
+func (p *mockCreator) CreateDB(_ context.Context, _ string, _ ...kivik.Options) error {
+	return nil
+}
+
+type errCreator struct {
+	backend
+}
+
+func (p *errCreator) CreateDB(_ context.Context, _ string, _ ...kivik.Options) error {
+	return errors.New("failure")
+}
+
 func TestPutDB(t *testing.T) {
-	_, h := createClient(t)
-	w := httptest.NewRecorder()
-	req := httptest.NewRequest("PUT", "/foo", nil)
-	handler := h.Main()
-	handler.ServeHTTP(w, req)
-	resp := w.Result()
-	defer resp.Body.Close()
-	expected := map[string]interface{}{
-		"ok": true,
-	}
-	if d := diff.AsJSON(expected, resp.Body); d != "" {
-		t.Error(d)
-	}
+	t.Run("Success", func(t *testing.T) {
+		h := &Handler{Client: &mockCreator{}}
+		resp := callEndpoint(h, "PUT", "/foo")
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected 200, got %s", resp.Status)
+		}
+		expected := map[string]interface{}{
+			"ok": true,
+		}
+		if d := diff.AsJSON(expected, resp.Body); d != "" {
+			t.Error(d)
+		}
+	})
+	t.Run("Error", func(t *testing.T) {
+		h := &Handler{Client: &errCreator{}}
+		resp := callEndpoint(h, "PUT", "/foo")
+		if resp.StatusCode != http.StatusInternalServerError {
+			t.Errorf("Expected 500, got %s", resp.Status)
+		}
+	})
+}
+
+type mockNotExists struct{ backend }
+
+func (d *mockNotExists) DBExists(_ context.Context, _ string, _ ...kivik.Options) (bool, error) {
+	return false, nil
+}
+
+type mockExists struct{ backend }
+
+func (d *mockExists) DBExists(_ context.Context, _ string, _ ...kivik.Options) (bool, error) {
+	return true, nil
+}
+
+type mockErrExists struct{ backend }
+
+func (d *mockErrExists) DBExists(_ context.Context, _ string, _ ...kivik.Options) (bool, error) {
+	return false, errors.New("failure")
 }
 
 func TestHeadDB(t *testing.T) {
-	client, h := createClient(t)
 	t.Run("NotExists", func(t *testing.T) {
+		h := &Handler{Client: &mockNotExists{}}
 		resp := callEndpoint(h, "HEAD", "/notexist")
 		if resp.StatusCode != http.StatusNotFound {
 			t.Errorf("Expected 404/NotFound, got %s", resp.Status)
 		}
 	})
+
 	t.Run("Exists", func(t *testing.T) {
-		if err := client.CreateDB(context.Background(), "exists"); err != nil {
-			t.Fatal(err)
-		}
+		h := &Handler{Client: &mockExists{}}
 		resp := callEndpoint(h, "HEAD", "/exists")
 		if resp.StatusCode != http.StatusOK {
 			t.Errorf("Expected 200/OK, got %s", resp.Status)
 		}
 	})
+
+	t.Run("Error", func(t *testing.T) {
+		h := &Handler{Client: &mockErrExists{}}
+		resp := callEndpoint(h, "HEAD", "/exists")
+		if resp.StatusCode != http.StatusInternalServerError {
+			t.Errorf("Expected 500, got %s", resp.Status)
+		}
+	})
 }
 
-type errClient struct {
-	*kivik.Client
+type mockGetFound struct{ backend }
+
+func (c *mockGetFound) DBExists(_ context.Context, _ string, _ ...kivik.Options) (bool, error) {
+	return true, nil
 }
+
+type mockGetNotFound struct{ backend }
+
+func (c *mockGetNotFound) DBExists(_ context.Context, _ string, _ ...kivik.Options) (bool, error) {
+	return false, nil
+}
+
+type errClient struct{ backend }
 
 func (c *errClient) DBExists(_ context.Context, _ string, _ ...kivik.Options) (bool, error) {
-	return false, errors.New("aaah!")
+	return false, errors.New("failure")
 }
 
 func TestGetDB(t *testing.T) {
-	client, h := createClient(t)
 	t.Run("Endpoint exists for GET", func(t *testing.T) {
+		h := &Handler{Client: &mockGetNotFound{}}
 		resp := callEndpoint(h, "GET", "/exists")
 		if resp.StatusCode == http.StatusMethodNotAllowed {
 			t.Error("Expected another response than method not allowed")
@@ -64,6 +122,7 @@ func TestGetDB(t *testing.T) {
 	})
 
 	t.Run("Not found", func(t *testing.T) {
+		h := &Handler{Client: &mockGetNotFound{}}
 		resp := callEndpoint(h, "GET", "/notexists")
 		if resp.StatusCode != http.StatusNotFound {
 			t.Errorf("Expected 404, got %s", resp.Status)
@@ -71,38 +130,20 @@ func TestGetDB(t *testing.T) {
 	})
 
 	t.Run("Found", func(t *testing.T) {
-		path := "exists"
-		if err := client.CreateDB(context.Background(), path); err != nil {
-			t.Fatal(err)
-		}
-		resp := callEndpoint(h, "GET", "/"+path)
+		h := &Handler{Client: &mockGetFound{}}
+		resp := callEndpoint(h, "GET", "/asdf")
 		if resp.StatusCode != http.StatusOK {
 			t.Errorf("Expected 200, got %s", resp.Status)
 		}
 	})
 
 	t.Run("Error", func(t *testing.T) {
-		myErrClient := errClient{client}
-
-		h2 := &Handler{Client: &myErrClient}
+		h2 := &Handler{Client: &errClient{}}
 		resp := callEndpoint(h2, "GET", "/error")
 		if resp.StatusCode != http.StatusInternalServerError {
 			t.Errorf("Expected 500, got %s", resp.Status)
 		}
 	})
-}
-
-func TestFlush(t *testing.T) {
-	// TODO
-}
-
-func createClient(t *testing.T) (*kivik.Client, *Handler) {
-	client, err := kivik.New(context.Background(), "memory", "")
-	if err != nil {
-		t.Fatal(err)
-	}
-	h := &Handler{Client: client}
-	return client, h
 }
 
 func callEndpoint(h *Handler, method string, path string) *http.Response {

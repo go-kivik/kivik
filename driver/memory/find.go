800806558194e5b26c55b763c283f6e5edeafa91
Jonathan Hall <flimzy@flimzy.com>
2017-07-23 14:51:46 +0200

    Add indexSpec for find queries

--- driver/memory/find.go
+++ driver/memory/find.go
@@ -2,12 +2,45 @@ package memory
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 
 	"github.com/flimzy/kivik/driver"
 )
 
 var errFindNotImplemented = errors.New("find feature not yet implemented")
+
+type query struct {
+	Selector map[string]interface{} `json:"selector"`
+	Limit    int64                  `json:"limit"`
+	Skip     int64                  `json:"skip"`
+	Sort     []string               `json:"sort"`
+	Fields   []string               `json:"fields"`
+	UseIndex indexSpec              `json:"use_index"`
+}
+
+type indexSpec struct {
+	ddoc  string
+	index string
+}
+
+func (i *indexSpec) UnmarshalJSON(data []byte) error {
+	if data[0] == '"' {
+		return json.Unmarshal(data, &i.ddoc)
+	}
+	var values []string
+	if err := json.Unmarshal(data, &values); err != nil {
+		return err
+	}
+	if len(values) == 0 || len(values) > 2 {
+		return errors.New("invalid index specification")
+	}
+	i.ddoc = values[0]
+	if len(values) == 2 {
+		i.index = values[1]
+	}
+	return nil
+}
 
 func (d *db) Find(_ context.Context, query interface{}) (driver.Rows, error) {
 	return nil, nil
--- driver/memory/find_test.go
+++ driver/memory/find_test.go
@@ -3,7 +3,69 @@ package memory
 import (
 	"context"
 	"testing"
+
+	"github.com/flimzy/diff"
 )
+
+func TestIndexSpecUnmarshalJSON(t *testing.T) {
+	type isuTest struct {
+		name     string
+		input    string
+		expected *indexSpec
+		err      string
+	}
+	tests := []isuTest{
+		{
+			name:     "ddoc only",
+			input:    `"foo"`,
+			expected: &indexSpec{ddoc: "foo"},
+		},
+		{
+			name:     "ddoc and index",
+			input:    `["foo","bar"]`,
+			expected: &indexSpec{ddoc: "foo", index: "bar"},
+		},
+		{
+			name:  "invalid json",
+			input: "asdf",
+			err:   "invalid character 'a' looking for beginning of value",
+		},
+		{
+			name:  "extra fields",
+			input: `["foo","bar","baz"]`,
+			err:   "invalid index specification",
+		},
+		{
+			name:     "One field",
+			input:    `["foo"]`,
+			expected: &indexSpec{ddoc: "foo"},
+		},
+		{
+			name:  "Empty array",
+			input: `[]`,
+			err:   "invalid index specification",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := &indexSpec{}
+			err := result.UnmarshalJSON([]byte(test.input))
+			var msg string
+			if err != nil {
+				msg = err.Error()
+			}
+			if msg != test.err {
+				t.Errorf("Unexpected error: %s", err)
+			}
+			if err != nil {
+				return
+			}
+			if d := diff.Interface(test.expected, result); d != "" {
+				t.Errorf(d)
+			}
+		})
+	}
+}
 
 func TestCreateIndex(t *testing.T) {
 	d := &db{}

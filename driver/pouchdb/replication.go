f7759994a4d542e63a31026e2707ceb98ed1701e
Jonathan Hall <flimzy@flimzy.com>
2017-08-16 20:22:24 +0200

    Report replication stats from PouchDB driver.

--- driver/pouchdb/replication.go
+++ driver/pouchdb/replication.go
@@ -81,6 +81,11 @@ func (r *replication) Update(ctx context.Context, state *driver.ReplicationInfo)
 			r.endTime = endTime
 		}
 	}
+	if r.rh.state != nil {
+		state.DocWriteFailures = r.rh.state.DocWriteFailures
+		state.DocsRead = r.rh.state.DocsRead
+		state.DocsWritten = r.rh.state.DocsWritten
+	}
 	return nil
 }
 

b7c7ad90a87277d027e460f038457ca5dd65359a
Jonathan Hall <flimzy@flimzy.com>
2017-08-19 19:31:39 +0200

    Add deprecation notice to legacy memory driver

--- driver/memory/memory.go
+++ driver/memory/memory.go
@@ -1,4 +1,6 @@
 // Package memory provides a memory-backed Kivik driver, intended for testing.
+// This version of the package is deprecated, and no longer receiving updates.
+// Please use github.com/go-kivik/memorydb instead.
 package memory
 
 import (

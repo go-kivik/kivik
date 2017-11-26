25585d9c7048de8e83080953dfd4a5e12c85816d
Jonathan Hall <flimzy@flimzy.com>
2017-11-26 14:45:50 +0100

    Add StatusCodeOK utility function

--- errors/errors.go
+++ errors/errors.go
@@ -63,10 +63,22 @@ func StatusCode(err error) int {
 	if err == nil {
 		return statusNoError
 	}
-	if scErr, ok := err.(statusCoder); ok {
-		return scErr.StatusCode()
+	if sc, ok := StatusCodeOK(err); ok {
+		return sc
 	}
 	return statusInternalServerError
+}
+
+// StatusCodeOK extracts an embedded HTTP status code from an error and returns
+// it. ok will be false if the error does not embed a status code.
+func StatusCodeOK(err error) (code int, ok bool) {
+	if err == nil {
+		return statusNoError, false
+	}
+	if scErr, ok := err.(statusCoder); ok {
+		return scErr.StatusCode(), true
+	}
+	return statusNoError, false
 }
 
 // Reasoner is an interface for an error that contains a reason.

d7e2eb84165c6e15f97952ce32ac39553fc29943
Jonathan Hall <flimzy@flimzy.com>
2017-12-03 17:04:37 +0100

    Clean up test script

--- travis/test.sh
+++ travis/test.sh
@@ -36,11 +36,11 @@ case "$1" in
         unset KIVIK_TEST_DSN_COUCH20
         echo "" > coverage.txt
 
-        TEST_PKGS=$(find -name "*_test.go" | grep -v /vendor/ | grep -v /pouchdb | xargs dirname | sort -u | sed -e "s#^\.#github.com/flimzy/kivik#" )
+        TEST_PKGS=$(find -name "*_test.go" | grep -v /vendor/ | xargs dirname | sort -u | sed -e "s#^\.#github.com/flimzy/kivik#" )
 
         for d in $TEST_PKGS; do
             go test -i $d
-            DEPS=$((go list -f $'{{range $f := .TestImports}}{{$f}}\n{{end}}{{range $f := .Imports}}{{$f}}\n{{end}}' $d && echo $d) | sort -u | grep -v /vendor/ | grep -v /pouchdb | grep -v driver/couchdb | grep -v driver/memory | grep -v /kivik/test | grep ^github.com/flimzy/kivik | tr '\n' ' ')
+            DEPS=$((go list -f $'{{range $f := .TestImports}}{{$f}}\n{{end}}{{range $f := .Imports}}{{$f}}\n{{end}}' $d && echo $d) | sort -u | grep -v /vendor/ | grep -v /kivik/test | grep ^github.com/flimzy/kivik | tr '\n' ' ')
             go test -coverprofile=profile.out -covermode=set -coverpkg=$(join_list $DEPS) $d
             if [ -f profile.out ]; then
                 cat profile.out >> coverage.txt

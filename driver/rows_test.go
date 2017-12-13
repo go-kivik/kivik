3a4e989aff065da5b7b87e754ece1fbe831fc78e
Jonathan Hall <flimzy@flimzy.com>
2017-12-13 11:03:05 +0100

    Tests for SequenceID unmarshaler

--- driver/rows_test.go
+++ driver/rows_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/flimzy/diff"
+	"github.com/flimzy/testy"
 )
 
 func TestChangesUnmarshal(t *testing.T) {
@@ -22,5 +23,36 @@ func TestChangesUnmarshal(t *testing.T) {
 	expected := []string{"6-460637e73a6288cb24d532bf91f32969", "5-eeaa298781f60b7bcae0c91bdedd1b87"}
 	if d := diff.AsJSON(expected, changes); d != nil {
 		t.Errorf("Results differ from expected:\n%s\n", d)
+	}
+}
+
+func TestSequenceIDUnmarshal(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+
+		expected SequenceID
+		err      string
+	}{
+		{
+			name:     "Couch 1.6",
+			input:    "123",
+			expected: "123",
+		},
+		{
+			name:     "Couch 2.0",
+			input:    `"1-seqfoo"`,
+			expected: "1-seqfoo",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var seq SequenceID
+			err := json.Unmarshal([]byte(test.input), &seq)
+			testy.Error(t, test.err, err)
+			if seq != test.expected {
+				t.Errorf("Unexpected result: %s", seq)
+			}
+		})
 	}
 }

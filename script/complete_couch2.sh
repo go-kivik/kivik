c391809fc0e706183d18b5ca9dd9091cd46cd729
Jonathan Hall <flimzy@flimzy.com>
2023-08-25 22:29:03 +0200

    Consolidate CI config

--- script/complete_couch2.sh
+++ script/complete_couch2.sh
@@ -10,9 +10,10 @@ for db in _users _replicator _global_changes; do
             # Already exists, nothing to do.
         ;;
         *)
-            echo "Failed to create %{db}"
+            echo "Failed to create ${db}: ${status}"
             exit 1
         ;;
     esac
 done
 curl --silent --fail -o /dev/null -X PUT ${1}/_node/nonode@nohost/_config/replicator/interval -d '"1000"'
+curl --silent --fail -o /dev/null -X PUT ${1}/_node/nonode@nohost/_config/cluster/n -d '"1"'

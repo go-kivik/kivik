dd21f661c6517cc1beb1a1f31d65eb114cd240c7
Jonathan Hall <flimzy@flimzy.com>
2023-09-24 22:07:32 +0200

    Extract rows type and tests to separate files

--- resultset.go
+++ resultset.go
@@ -13,15 +13,11 @@
 package kivik
 
 import (
-	"context"
-	"encoding/json"
 	"errors"
 	"io"
-	"net/http"
 	"reflect"
 
 	"github.com/go-kivik/kivik/v4/driver"
-	"github.com/go-kivik/kivik/v4/internal"
 )
 
 // ResultMetadata contains metadata about certain queries.
@@ -251,37 +247,6 @@ type fullResultSet interface {
 	Key() (string, error)
 }
 
-type rows struct {
-	*iter
-	rowsi driver.Rows
-}
-
-var _ fullResultSet = &rows{}
-
-func (r *rows) NextResultSet() bool {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
-	if r.err != nil {
-		return false
-	}
-	if r.state == stateClosed {
-		return false
-	}
-	if r.state == stateRowReady {
-		r.err = errors.New("must call NextResultSet before Next")
-		return false
-	}
-	r.state = stateResultSetReady
-	return true
-}
-
-func (r *rows) Metadata() (*ResultMetadata, error) {
-	for r.iter == nil || (r.state != stateEOQ && r.state != stateClosed) {
-		return nil, &internal.Error{Status: http.StatusBadRequest, Err: errors.New("Metadata must not be called until result set iteration is complete")}
-	}
-	return r.feed.(*rowsIterator).ResultMetadata, nil
-}
-
 type rowsIterator struct {
 	driver.Rows
 	*ResultMetadata
@@ -308,45 +273,6 @@ func (r *rowsIterator) Next(i interface{}) error {
 		}
 	}
 	return err
-}
-
-func newRows(ctx context.Context, onClose func(), rowsi driver.Rows) *rows {
-	return &rows{
-		iter:  newIterator(ctx, onClose, &rowsIterator{Rows: rowsi}, &driver.Row{}),
-		rowsi: rowsi,
-	}
-}
-
-func (r *rows) ScanValue(dest interface{}) (err error) {
-	runlock, err := r.makeReady(&err)
-	if err != nil {
-		return err
-	}
-	defer runlock()
-	row := r.curVal.(*driver.Row)
-	if row.Error != nil {
-		return row.Error
-	}
-	if row.Value != nil {
-		return json.NewDecoder(row.Value).Decode(dest)
-	}
-	return nil
-}
-
-func (r *rows) ScanDoc(dest interface{}) (err error) {
-	runlock, err := r.makeReady(&err)
-	if err != nil {
-		return err
-	}
-	defer runlock()
-	row := r.curVal.(*driver.Row)
-	if err := row.Error; err != nil {
-		return err
-	}
-	if row.Doc != nil {
-		return json.NewDecoder(row.Doc).Decode(dest)
-	}
-	return &internal.Error{Status: http.StatusBadRequest, Message: "kivik: doc is nil; does the query include docs?"}
 }
 
 // ScanAllDocs loops through the remaining documents in the resultset, and scans
@@ -416,58 +342,4 @@ func scanAll(r fullResultSet, dest interface{}, scan func(interface{}) error) (e
 		}
 	}
 	return nil
-}
-
-func (r *rows) ScanKey(dest interface{}) (err error) {
-	runlock, err := r.makeReady(&err)
-	if err != nil {
-		return err
-	}
-	defer runlock()
-	row := r.curVal.(*driver.Row)
-	if err := json.Unmarshal(row.Key, dest); err != nil {
-		return err
-	}
-	return row.Error
-}
-
-func (r *rows) ID() (string, error) {
-	runlock, err := r.makeReady(nil)
-	if err != nil {
-		return "", err
-	}
-	defer runlock()
-	row := r.curVal.(*driver.Row)
-	return row.ID, row.Error
-}
-
-func (r *rows) Key() (string, error) {
-	runlock, err := r.makeReady(nil)
-	if err != nil {
-		return "", err
-	}
-	defer runlock()
-	row := r.curVal.(*driver.Row)
-	return string(row.Key), row.Error
-}
-
-func (r *rows) Attachments() (*AttachmentsIterator, error) {
-	runlock, err := r.makeReady(nil)
-	if err != nil {
-		return nil, err
-	}
-	defer runlock()
-	row := r.curVal.(*driver.Row)
-	if row.Error != nil {
-		return nil, row.Error
-	}
-	if row.Attachments == nil {
-		return nil, nil // TODO: should this return an error?
-	}
-	return &AttachmentsIterator{atti: row.Attachments}, nil
-}
-
-func (r *rows) Rev() (string, error) {
-	row := r.curVal.(*driver.Row)
-	return row.Rev, row.Error
 }
--- resultset_test.go
+++ resultset_test.go
@@ -17,7 +17,6 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
-	"net/http"
 	"strings"
 	"testing"
 
@@ -26,61 +25,6 @@ import (
 	"github.com/go-kivik/kivik/v4/driver"
 	"github.com/go-kivik/kivik/v4/internal/mock"
 )
-
-func TestRowsNext(t *testing.T) {
-	tests := []struct {
-		name     string
-		rows     *rows
-		expected bool
-	}{
-		{
-			name: "nothing more",
-			rows: &rows{
-				iter: &iter{state: stateClosed},
-			},
-			expected: false,
-		},
-		{
-			name: "more",
-			rows: &rows{
-				iter: &iter{
-					feed: &mockIterator{
-						NextFunc: func(_ interface{}) error { return nil },
-					},
-				},
-			},
-			expected: true,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			result := test.rows.Next()
-			if result != test.expected {
-				t.Errorf("Unexpected result: %v", result)
-			}
-		})
-	}
-}
-
-func TestRowsErr(t *testing.T) {
-	const expected = "foo error"
-	r := &rows{
-		iter: &iter{err: errors.New(expected)},
-	}
-	err := r.Err()
-	testy.Error(t, expected, err)
-}
-
-func TestRowsClose(t *testing.T) {
-	const expected = "close error"
-	r := &rows{
-		iter: &iter{
-			feed: &mockIterator{CloseFunc: func() error { return errors.New(expected) }},
-		},
-	}
-	err := r.Close()
-	testy.Error(t, expected, err)
-}
 
 func TestRowsIteratorNext(t *testing.T) {
 	const expected = "foo error"
@@ -94,514 +38,113 @@ func TestRowsIteratorNext(t *testing.T) {
 	testy.Error(t, expected, err)
 }
 
-func TestRowsScanValue(t *testing.T) {
-	type tt struct {
-		rows     *rows
-		expected interface{}
-		state    int
-		status   int
-		err      string
-	}
+func TestNextResultSet(t *testing.T) {
+	t.Run("two resultsets", func(t *testing.T) {
+		r := multiResultSet()
 
-	tests := testy.NewTable()
-	tests.Add("success", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Value: strings.NewReader(`{"foo":123.4}`),
-				},
-			},
-		},
-		state:    stateRowReady,
-		expected: map[string]interface{}{"foo": 123.4},
-	})
-	tests.Add("one item", func() interface{} {
-		rowsi := &mock.Rows{
-			NextFunc: func(r *driver.Row) error {
-				r.Value = strings.NewReader(`"foo"`)
-				return nil
-			},
+		ids := []string{}
+		for r.NextResultSet() {
+			for r.Next() {
+				id, _ := r.ID()
+				ids = append(ids, id)
+			}
 		}
-		return tt{
-			rows:     newRows(context.Background(), nil, rowsi),
-			expected: "foo",
-			state:    stateClosed,
+		if err := r.Err(); err != nil {
+			t.Error(err)
 		}
-	})
-	tests.Add("closed", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateClosed,
-				curVal: &driver.Row{
-					Value: strings.NewReader(`{"foo":123.4}`),
-				},
-			},
-		},
-		expected: map[string]interface{}{"foo": 123.4},
-		state:    stateClosed,
-	})
-	tests.Add("row error", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Error: errors.New("row error"),
-				},
-			},
-		},
-		status: 500,
-		err:    "row error",
-	})
-
-	tests.Run(t, func(t *testing.T, tt tt) {
-		var result interface{}
-		err := tt.rows.ScanValue(&result)
-		testy.StatusError(t, tt.err, tt.status, err)
-		if d := testy.DiffInterface(tt.expected, result); d != nil {
+		want := []string{"1", "2", "3", "x", "y"}
+		if d := testy.DiffInterface(want, ids); d != nil {
 			t.Error(d)
 		}
-		if tt.state != tt.rows.state {
-			t.Errorf("Unexpected state: %v", tt.rows.state)
+	})
+	t.Run("called out of order", func(t *testing.T) {
+		r := multiResultSet()
+
+		if !r.Next() {
+			t.Fatal("expected next to return true")
 		}
-	})
-}
-
-func TestRowsScanDoc(t *testing.T) {
-	type tt struct {
-		rows     *rows
-		expected interface{}
-		state    int
-		status   int
-		err      string
-	}
-
-	tests := testy.NewTable()
-
-	tests.Add("old row", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Doc: strings.NewReader(`{"foo":123.4}`),
-				},
-			},
-		},
-		state:    stateRowReady,
-		expected: map[string]interface{}{"foo": 123.4},
-	})
-	tests.Add("success", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Doc: strings.NewReader(`{"foo":123.4}`),
-				},
-			},
-		},
-		state:    stateRowReady,
-		expected: map[string]interface{}{"foo": 123.4},
-	})
-	tests.Add("one item", func() interface{} {
-		rowsi := &mock.Rows{
-			NextFunc: func(r *driver.Row) error {
-				r.Doc = strings.NewReader(`{"foo":"bar"}`)
-				return nil
-			},
-		}
-		return tt{
-			rows:     newRows(context.Background(), nil, rowsi),
-			expected: map[string]interface{}{"foo": "bar"},
-			state:    stateClosed,
-		}
-	})
-	tests.Add("closed", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateClosed,
-				curVal: &driver.Row{
-					Doc: strings.NewReader(`{"foo":123.4}`),
-				},
-			},
-		},
-		state:    stateClosed,
-		expected: map[string]interface{}{"foo": 123.4},
-	})
-	tests.Add("nil doc", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Doc: nil,
-				},
-			},
-		},
-		status: http.StatusBadRequest,
-		err:    "kivik: doc is nil; does the query include docs?",
-	})
-	tests.Add("row error", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Error: errors.New("row error"),
-				},
-			},
-		},
-		status: 500,
-		err:    "row error",
-	})
-
-	tests.Run(t, func(t *testing.T, tt tt) {
-		var result interface{}
-		err := tt.rows.ScanDoc(&result)
-		testy.StatusError(t, tt.err, tt.status, err)
-		if d := testy.DiffInterface(tt.expected, result); d != nil {
-			t.Error(d)
-		}
-		if tt.state != tt.rows.state {
-			t.Errorf("Unexpected state: %v", tt.rows.state)
-		}
-	})
-}
-
-func TestRowsScanKey(t *testing.T) {
-	type tt struct {
-		rows     *rows
-		expected interface{}
-		state    int
-		status   int
-		err      string
-	}
-
-	tests := testy.NewTable()
-	tests.Add("success", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Key: []byte(`{"foo":123.4}`),
-				},
-			},
-		},
-		state:    stateRowReady,
-		expected: map[string]interface{}{"foo": 123.4},
-	})
-	tests.Add("one item", func() interface{} {
-		rowsi := &mock.Rows{
-			NextFunc: func(r *driver.Row) error {
-				r.Key = []byte(`"foo"`)
-				return nil
-			},
-		}
-		return tt{
-			rows:     newRows(context.Background(), nil, rowsi),
-			expected: "foo",
-			state:    stateClosed,
-		}
-	})
-	tests.Add("closed", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateClosed,
-				curVal: &driver.Row{
-					Key: []byte(`"foo"`),
-				},
-			},
-		},
-		state:    stateClosed,
-		expected: "foo",
-	})
-	tests.Add("row error", tt{
-		rows: &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					Key:   json.RawMessage(`"id"`),
-					Error: errors.New("row error"),
-				},
-			},
-		},
-		expected: "id",
-		state:    stateRowReady,
-		status:   http.StatusInternalServerError,
-		err:      "row error",
-	})
-
-	tests.Run(t, func(t *testing.T, tt tt) {
-		var result interface{}
-		err := tt.rows.ScanKey(&result)
-		if !testy.ErrorMatches(tt.err, err) {
-			t.Errorf("unexpected error: %s", err)
-		}
-		if status := HTTPStatus(err); status != tt.status {
-			t.Errorf("Unexpected error status: %v", status)
-		}
-		if d := testy.DiffInterface(tt.expected, result); d != nil {
-			t.Error(d)
-		}
-		if tt.state != tt.rows.state {
-			t.Errorf("Unexpected state: %v", tt.rows.state)
-		}
-	})
-}
-
-func TestRowsGetters(t *testing.T) {
-	id := "foo"
-	key := []byte("[1234]")
-	offset := int64(2)
-	totalrows := int64(3)
-	updateseq := "asdfasdf"
-	r := &rows{
-		iter: &iter{
-			state: stateRowReady,
-			curVal: &driver.Row{
-				ID:  id,
-				Key: key,
-			},
-		},
-		rowsi: &mock.Rows{
-			OffsetFunc:    func() int64 { return offset },
-			TotalRowsFunc: func() int64 { return totalrows },
-			UpdateSeqFunc: func() string { return updateseq },
-		},
-	}
-
-	t.Run("ID", func(t *testing.T) {
-		result, _ := r.ID()
-		if id != result {
-			t.Errorf("Unexpected result: %v", result)
-		}
-	})
-
-	t.Run("Key", func(t *testing.T) {
-		result, _ := r.Key()
-		if string(key) != result {
-			t.Errorf("Unexpected result: %v", result)
-		}
-	})
-
-	t.Run("Not Ready", func(t *testing.T) {
-		t.Run("ID", func(t *testing.T) {
-			rowsi := &mock.Rows{
-				NextFunc: func(r *driver.Row) error {
-					r.ID = id
-					return nil
-				},
-			}
-			r := newRows(context.Background(), nil, rowsi)
-
-			result, _ := r.ID()
-			if result != id {
-				t.Errorf("Unexpected result: %v", result)
-			}
-		})
-
-		t.Run("Key", func(t *testing.T) {
-			rowsi := &mock.Rows{
-				NextFunc: func(r *driver.Row) error {
-					r.Key = key
-					return nil
-				},
-			}
-			r := newRows(context.Background(), nil, rowsi)
-
-			result, _ := r.Key()
-			if result != string(key) {
-				t.Errorf("Unexpected result: %v", result)
-			}
-		})
-	})
-
-	t.Run("after close", func(t *testing.T) {
-		rowsi := &mock.Rows{
-			OffsetFunc:    func() int64 { return offset },
-			TotalRowsFunc: func() int64 { return totalrows },
-			UpdateSeqFunc: func() string { return updateseq },
-		}
-		r := &rows{
-			iter: &iter{
-				state: stateRowReady,
-				curVal: &driver.Row{
-					ID:  id,
-					Key: key,
-				},
-				feed: &rowsIterator{Rows: rowsi},
-			},
-			rowsi: rowsi,
+		if r.NextResultSet() {
+			t.Fatal("expected NextResultSet to return false")
 		}
 
-		if err := r.Close(); err != nil {
-			t.Fatal(err)
-		}
-
-		t.Run("ID", func(t *testing.T) {
-			result, _ := r.ID()
-			if id != result {
-				t.Errorf("Unexpected result: %v", result)
-			}
-		})
-
-		t.Run("Key", func(t *testing.T) {
-			result, _ := r.Key()
-			if string(key) != result {
-				t.Errorf("Unexpected result: %v", result)
-			}
-		})
-
-		t.Run("ScanKey", func(t *testing.T) {
-			var result json.RawMessage
-			err := r.ScanKey(&result)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if string(result) != string(key) {
-				t.Errorf("Unexpected result: %v", result)
-			}
-		})
-	})
-}
-
-func TestMetadata(t *testing.T) {
-	t.Run("iteration incomplete", func(t *testing.T) {
-		r := newRows(context.Background(), nil, &mock.Rows{
-			OffsetFunc:    func() int64 { return 123 },
-			TotalRowsFunc: func() int64 { return 234 },
-			UpdateSeqFunc: func() string { return "seq" },
-		})
-		_, err := r.Metadata()
-		wantErr := "Metadata must not be called until result set iteration is complete"
+		wantErr := "must call NextResultSet before Next"
+		err := r.Err()
 		if !testy.ErrorMatches(wantErr, err) {
 			t.Errorf("Unexpected error: %s", err)
 		}
 	})
+	t.Run("next only", func(t *testing.T) {
+		r := multiResultSet()
 
-	check := func(t *testing.T, r fullResultSet) {
-		t.Helper()
-		for r.Next() { //nolint:revive // Consume all rows
+		ids := []string{}
+		for r.Next() {
+			id, _ := r.ID()
+			ids = append(ids, id)
 		}
-		meta, err := r.Metadata()
-		if err != nil {
-			t.Fatal(err)
+		if err := r.Err(); err != nil {
+			t.Error(err)
 		}
-		if d := testy.DiffInterface(testy.Snapshot(t), meta); d != nil {
+		want := []string{"1", "2", "3", "x", "y"}
+		if d := testy.DiffInterface(want, ids); d != nil {
 			t.Error(d)
 		}
+	})
+	t.Run("don't call NextResultSet in loop", func(t *testing.T) {
+		r := multiResultSet()
+
+		ids := []string{}
+		r.NextResultSet()
+		for r.Next() {
+			id, _ := r.ID()
+			ids = append(ids, id)
+		}
+		_ = r.Next() // once more to ensure it doesn't error past the end of the first RS
+		if err := r.Err(); err != nil {
+			t.Error(err)
+		}
+
+		// Only the first result set is processed, since NextResultSet is never
+		// called a second time.
+		want := []string{"1", "2", "3"}
+		if d := testy.DiffInterface(want, ids); d != nil {
+			t.Error(d)
+		}
+	})
+}
+
+func multiResultSet() fullResultSet {
+	rows := []interface{}{
+		&driver.Row{ID: "1", Doc: strings.NewReader(`{"foo":"bar"}`)},
+		&driver.Row{ID: "2", Doc: strings.NewReader(`{"foo":"bar"}`)},
+		&driver.Row{ID: "3", Doc: strings.NewReader(`{"foo":"bar"}`)},
+		int64(5),
+		&driver.Row{ID: "x", Doc: strings.NewReader(`{"foo":"bar"}`)},
+		&driver.Row{ID: "y", Doc: strings.NewReader(`{"foo":"bar"}`)},
+		int64(2),
 	}
+	var offset int64
 
-	t.Run("Standard", func(t *testing.T) {
-		r := newRows(context.Background(), nil, &mock.Rows{
-			OffsetFunc:    func() int64 { return 123 },
-			TotalRowsFunc: func() int64 { return 234 },
-			UpdateSeqFunc: func() string { return "seq" },
-		})
-		check(t, r)
-	})
-	t.Run("Bookmarker", func(t *testing.T) {
-		expected := "test bookmark"
-		r := newRows(context.Background(), nil, &mock.Bookmarker{
-			BookmarkFunc: func() string { return expected },
-		})
-		check(t, r)
-	})
-	t.Run("Warner", func(t *testing.T) {
-		expected := "test warning"
-		r := newRows(context.Background(), nil, &mock.RowsWarner{
-			WarningFunc: func() string { return expected },
-		})
-		check(t, r)
-	})
-	t.Run("query in progress", func(t *testing.T) {
-		rows := []interface{}{
-			&driver.Row{Doc: strings.NewReader(`{"foo":"bar"}`)},
-			&driver.Row{Doc: strings.NewReader(`{"foo":"bar"}`)},
-			&driver.Row{Doc: strings.NewReader(`{"foo":"bar"}`)},
-		}
-
-		r := newRows(context.Background(), nil, &mock.Rows{
-			NextFunc: func(r *driver.Row) error {
-				if len(rows) == 0 {
-					return io.EOF
-				}
-				if dr, ok := rows[0].(*driver.Row); ok {
-					rows = rows[1:]
-					*r = *dr
-					return nil
-				}
-				return driver.EOQ
-			},
-			OffsetFunc: func() int64 {
-				return 5
-			},
-		})
-		var i int
-		for r.Next() {
-			i++
-			if i > 10 {
-				panic(i)
+	return newRows(context.Background(), nil, &mock.Rows{
+		NextFunc: func(r *driver.Row) error {
+			if len(rows) == 0 {
+				return io.EOF
 			}
-		}
-		check(t, r)
-	})
-	t.Run("no query in progress", func(t *testing.T) {
-		rows := []interface{}{
-			&driver.Row{Doc: strings.NewReader(`{"foo":"bar"}`)},
-			&driver.Row{Doc: strings.NewReader(`{"foo":"bar"}`)},
-			&driver.Row{Doc: strings.NewReader(`{"foo":"bar"}`)},
-		}
-
-		r := newRows(context.Background(), nil, &mock.Rows{
-			NextFunc: func(r *driver.Row) error {
-				if len(rows) == 0 {
-					return io.EOF
-				}
-				if dr, ok := rows[0].(*driver.Row); ok {
-					rows = rows[1:]
-					*r = *dr
-					return nil
-				}
+			row := rows[0]
+			rows = rows[1:]
+			switch t := row.(type) {
+			case *driver.Row:
+				*r = *t
+				return nil
+			case int64:
+				offset = t
 				return driver.EOQ
-			},
-			OffsetFunc: func() int64 {
-				return 5
-			},
-		})
-		check(t, r)
-	})
-	t.Run("followed by other query in resultset mode", func(t *testing.T) {
-		r := multiResultSet()
-
-		_ = r.NextResultSet()
-		check(t, r)
-		ids := []string{}
-		for r.Next() {
-			id, _ := r.ID()
-			ids = append(ids, id)
-		}
-		want := []string{"x", "y"}
-		if d := testy.DiffInterface(want, ids); d != nil {
-			t.Error(d)
-		}
-		t.Run("second query", func(t *testing.T) {
-			check(t, r)
-		})
-	})
-	t.Run("followed by other query in row mode", func(t *testing.T) {
-		r := multiResultSet()
-
-		check(t, r)
-		ids := []string{}
-		for r.Next() {
-			id, _ := r.ID()
-			ids = append(ids, id)
-		}
-		want := []string{}
-		if d := testy.DiffInterface(want, ids); d != nil {
-			t.Error(d)
-		}
-		t.Run("second query", func(t *testing.T) {
-			check(t, r)
-		})
+			default:
+				panic("unknown type")
+			}
+		},
+		OffsetFunc: func() int64 {
+			return offset
+		},
 	})
 }
 
@@ -719,133 +262,4 @@ func TestScanAllDocs(t *testing.T) {
 			t.Error(d)
 		}
 	})
-}
-
-func TestNextResultSet(t *testing.T) {
-	t.Run("two resultsets", func(t *testing.T) {
-		r := multiResultSet()
-
-		ids := []string{}
-		for r.NextResultSet() {
-			for r.Next() {
-				id, _ := r.ID()
-				ids = append(ids, id)
-			}
-		}
-		if err := r.Err(); err != nil {
-			t.Error(err)
-		}
-		want := []string{"1", "2", "3", "x", "y"}
-		if d := testy.DiffInterface(want, ids); d != nil {
-			t.Error(d)
-		}
-	})
-	t.Run("called out of order", func(t *testing.T) {
-		r := multiResultSet()
-
-		if !r.Next() {
-			t.Fatal("expected next to return true")
-		}
-		if r.NextResultSet() {
-			t.Fatal("expected NextResultSet to return false")
-		}
-
-		wantErr := "must call NextResultSet before Next"
-		err := r.Err()
-		if !testy.ErrorMatches(wantErr, err) {
-			t.Errorf("Unexpected error: %s", err)
-		}
-	})
-	t.Run("next only", func(t *testing.T) {
-		r := multiResultSet()
-
-		ids := []string{}
-		for r.Next() {
-			id, _ := r.ID()
-			ids = append(ids, id)
-		}
-		if err := r.Err(); err != nil {
-			t.Error(err)
-		}
-		want := []string{"1", "2", "3", "x", "y"}
-		if d := testy.DiffInterface(want, ids); d != nil {
-			t.Error(d)
-		}
-	})
-	t.Run("don't call NextResultSet in loop", func(t *testing.T) {
-		r := multiResultSet()
-
-		ids := []string{}
-		r.NextResultSet()
-		for r.Next() {
-			id, _ := r.ID()
-			ids = append(ids, id)
-		}
-		_ = r.Next() // once more to ensure it doesn't error past the end of the first RS
-		if err := r.Err(); err != nil {
-			t.Error(err)
-		}
-
-		// Only the first result set is processed, since NextResultSet is never
-		// called a second time.
-		want := []string{"1", "2", "3"}
-		if d := testy.DiffInterface(want, ids); d != nil {
-			t.Error(d)
-		}
-	})
-}
-
-func multiResultSet() fullResultSet {
-	rows := []interface{}{
-		&driver.Row{ID: "1", Doc: strings.NewReader(`{"foo":"bar"}`)},
-		&driver.Row{ID: "2", Doc: strings.NewReader(`{"foo":"bar"}`)},
-		&driver.Row{ID: "3", Doc: strings.NewReader(`{"foo":"bar"}`)},
-		int64(5),
-		&driver.Row{ID: "x", Doc: strings.NewReader(`{"foo":"bar"}`)},
-		&driver.Row{ID: "y", Doc: strings.NewReader(`{"foo":"bar"}`)},
-		int64(2),
-	}
-	var offset int64
-
-	return newRows(context.Background(), nil, &mock.Rows{
-		NextFunc: func(r *driver.Row) error {
-			if len(rows) == 0 {
-				return io.EOF
-			}
-			row := rows[0]
-			rows = rows[1:]
-			switch t := row.(type) {
-			case *driver.Row:
-				*r = *t
-				return nil
-			case int64:
-				offset = t
-				return driver.EOQ
-			default:
-				panic("unknown type")
-			}
-		},
-		OffsetFunc: func() int64 {
-			return offset
-		},
-	})
-}
-
-func Test_bug576(t *testing.T) {
-	rows := newRows(context.Background(), nil, &mock.Rows{
-		NextFunc: func(*driver.Row) error {
-			return io.EOF
-		},
-	})
-
-	var result interface{}
-	err := rows.ScanDoc(&result)
-	wantErr := "no results"
-	wantStatus := http.StatusNotFound
-	if !testy.ErrorMatches(wantErr, err) {
-		t.Errorf("unexpected error: %s", err)
-	}
-	if status := HTTPStatus(err); status != wantStatus {
-		t.Errorf("Unexpected error status: %v", status)
-	}
 }

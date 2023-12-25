c3ed911abcec2386811a6afb36ab42c5cc4de064
Jonathan Hall <flimzy@flimzy.com>
2023-12-25 20:14:59 +0100

    Configure docker-compose to start CouchDB for tests

--- script/complete_couch2.sh
+++ script/complete_couch2.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 for db in _users _replicator _global_changes; do
-    status=$(curl --silent --write-out %{http_code} -o /dev/null -X PUT ${1}/${db})
+echo ${1}/${db}
+    status=$(curl --silent --write-out "%{http_code}" -o /dev/null -u ${COUCHDB_USER}:${COUCHDB_PASSWORD} -X PUT "${1}/${db}")
     case ${status} in
         2*)
             # Success!
@@ -15,5 +16,5 @@ for db in _users _replicator _global_changes; do
         ;;
     esac
 done
-curl --silent --fail -o /dev/null -X PUT ${1}/_node/nonode@nohost/_config/replicator/interval -d '"1000"'
-curl --silent --fail -o /dev/null -X PUT ${1}/_node/nonode@nohost/_config/cluster/n -d '"1"'
+curl --silent --fail -o /dev/null -u ${COUCHDB_USER}:${COUCHDB_PASSWORD} -X PUT "${1}/_node/nonode@nohost/_config/replicator/interval" -d '"1000"'
+curl --silent --fail -o /dev/null -u ${COUCHDB_USER}:${COUCHDB_PASSWORD} -X PUT "${1}/_node/nonode@nohost/_config/cluster/n" -d '"1"'

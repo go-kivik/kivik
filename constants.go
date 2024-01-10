d7ab828e5311b9292693099c7ad42919c2473029
Jonathan Hall <flimzy@flimzy.com>
2024-01-11 00:26:45 +0100

    Tag v4.2.0

--- constants.go
+++ constants.go
@@ -14,7 +14,7 @@ package kivik
 
 const (
 	// Version is the version of the Kivik library.
-	Version = "4.1.0"
+	Version = "4.2.0"
 )
 
 // SessionCookieName is the name of the CouchDB session cookie.

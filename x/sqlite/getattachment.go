5cc821505ea4d276dc14dc6ebf1614edefbfc95e
Jonathan Hall <flimzy@flimzy.com>
2024-02-26 16:50:26 +0100

    Return attachment metadata

--- x/sqlite/getattachment.go
+++ x/sqlite/getattachment.go
@@ -2,6 +2,8 @@ package sqlite
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -10,21 +12,23 @@ import (
 )
 
 func (d *db) GetAttachment(ctx context.Context, docID string, filename string, _ driver.Options) (*driver.Attachment, error) {
-	var found bool
-
-	err := d.db.QueryRowContext(ctx, fmt.Sprintf(`
-		SELECT EXISTS (
-			SELECT 1
-			FROM %[1]q AS att
-			WHERE att.id = $1
-			AND att.filename = $2
-		)
-		`, d.name+"_attachments"), docID, filename).Scan(&found)
+	attachment, err := d.attachmentExists(ctx, docID, filename)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, &internal.Error{Status: http.StatusNotFound, Message: "Not Found: missing"}
+	}
 	if err != nil {
 		return nil, err
 	}
-	if found {
-		return &driver.Attachment{}, nil
-	}
-	return nil, &internal.Error{Status: http.StatusNotFound, Message: "Not Found: missing"}
+
+	return attachment, nil
+}
+
+func (d *db) attachmentExists(ctx context.Context, docID string, filename string) (*driver.Attachment, error) {
+	var att driver.Attachment
+	err := d.db.QueryRowContext(ctx, fmt.Sprintf(`
+		SELECT filename, content_type, length, rev
+		FROM %s
+		WHERE id = $1 AND filename = $2
+		`, d.name+"_attachments"), docID, filename).Scan(&att.Filename, &att.ContentType, &att.Size, &att.RevPos)
+	return &att, err
 }
--- x/sqlite/getattachment_test.go
+++ x/sqlite/getattachment_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"gitlab.com/flimzy/testy"
 
 	"github.com/go-kivik/kivik/v4"
@@ -14,12 +15,18 @@ import (
 
 func TestDBGetAttachment(t *testing.T) {
 	t.Parallel()
+	type attachmentMetadata struct {
+		Filename    string
+		ContentType string
+		Length      int64
+		RevPos      int64
+	}
 	type test struct {
 		setup    func(t *testing.T, db driver.DB)
 		docID    string
 		filename string
 
-		wantAttachment *driver.Attachment
+		wantAttachment *attachmentMetadata
 		wantStatus     int
 		wantErr        string
 	}
@@ -31,7 +38,7 @@ func TestDBGetAttachment(t *testing.T) {
 		wantStatus: http.StatusNotFound,
 		wantErr:    "Not Found: missing",
 	})
-	tests.Add("when document exists and file exists we get the valid status", test{
+	tests.Add("when the attachment exists, return it", test{
 		setup: func(t *testing.T, db driver.DB) {
 			_, err := db.Put(context.Background(), "foo", map[string]interface{}{
 				"_id": "foo",
@@ -49,6 +56,33 @@ func TestDBGetAttachment(t *testing.T) {
 		docID:    "foo",
 		filename: "foo.txt",
 	})
+	tests.Add("when an attachment is returned, it contains metadata...", test{
+		setup: func(t *testing.T, db driver.DB) {
+			_, err := db.Put(context.Background(), "foo", map[string]interface{}{
+				"_id": "foo",
+				"_attachments": map[string]interface{}{
+					"foo.txt": map[string]interface{}{
+						"content_type": "text/plain",
+						"data":         "VGhpcyBpcyBhIGJhc2U2NCBlbmNvZGluZw==",
+					},
+				},
+			}, mock.NilOption)
+			if err != nil {
+				t.Fatal(err)
+			}
+		},
+		docID:    "foo",
+		filename: "foo.txt",
+		wantAttachment: &attachmentMetadata{
+			Filename:    "foo.txt",
+			ContentType: "text/plain",
+			Length:      25,
+			RevPos:      1,
+		},
+	})
+	// GetAttachment returns the latest revision by default
+	//
+
 	/*
 		TODO:
 		- doc exists, and file exists, but doc is deleted
@@ -56,6 +90,15 @@ func TestDBGetAttachment(t *testing.T) {
 		- return existing file from existing doc
 		- request attachment from historical revision
 		- failure: request attachment from historical revision that does not exist
+
+
+
+		- GetAttachment returns 404 when the document does exist, but the attachment has never existed
+		- GetAttachment returns 404 when the document has never existed
+		- GetAttachment returns 404 when the document was deleted
+		- GetAttachment returns 404 when the latest revision was deleted
+		- GetAttachment returns 404 when the document does exist, but the attachment has been deleted
+		- GetAttachment returns the latest revision
 	*/
 
 	tests.Run(t, func(t *testing.T, tt test) {
@@ -68,12 +111,25 @@ func TestDBGetAttachment(t *testing.T) {
 		// if opts == nil {
 		opts := mock.NilOption
 		// }
-		_, err := db.GetAttachment(context.Background(), tt.docID, tt.filename, opts)
+		attachment, err := db.GetAttachment(context.Background(), tt.docID, tt.filename, opts)
 		if !testy.ErrorMatches(tt.wantErr, err) {
 			t.Errorf("Unexpected error: %s", err)
 		}
 		if status := kivik.HTTPStatus(err); status != tt.wantStatus {
 			t.Errorf("Unexpected status: %d", status)
+		}
+
+		if tt.wantAttachment == nil {
+			return
+		}
+		got := &attachmentMetadata{
+			Filename:    attachment.Filename,
+			ContentType: attachment.ContentType,
+			Length:      attachment.Size,
+			RevPos:      attachment.RevPos,
+		}
+		if d := cmp.Diff(tt.wantAttachment, got); d != "" {
+			t.Errorf("Unexpected attachment metadata:\n%s", d)
 		}
 	})
 }

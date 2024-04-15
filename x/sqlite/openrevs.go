d59a90f0f185e86da80215199231bb90b0ae417e
Jonathan Hall <flimzy@flimzy.com>
2024-04-15 11:17:22 +0200

    OpenRevs: all with valid doc works

--- x/sqlite/openrevs.go
+++ x/sqlite/openrevs.go
@@ -16,6 +16,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"io"
 	"net/http"
 
 	"github.com/go-kivik/kivik/v4/driver"
@@ -24,25 +25,92 @@ import (
 
 func (d *db) OpenRevs(ctx context.Context, docID string, revs []string, _ driver.Options) (driver.Rows, error) {
 	if len(revs) == 1 && revs[0] == "all" {
-		tx, err := d.db.Begin()
+		query := d.query(`
+			SELECT
+				leaf.rev || '-' || leaf.rev_id AS rev,
+				docs.deleted,
+				docs.doc
+			FROM (
+				SELECT
+					parent.id,
+					parent.rev,
+					parent.rev_id
+				FROM {{ .Revs }} AS parent
+				LEFT JOIN {{ .Revs }} AS child ON parent.id = child.id AND parent.rev = child.parent_rev AND parent.rev_id = child.parent_rev_id
+				WHERE parent.id = $1 AND child.id IS NULL
+			) AS leaf
+			JOIN {{ .Docs }} AS docs ON leaf.id = docs.id AND leaf.rev = docs.rev AND leaf.rev_id = docs.rev_id
+		`)
+		rows, err := d.db.QueryContext(ctx, query, docID) //nolint:rowserrcheck // Err checked in Next
 		if err != nil {
 			return nil, err
 		}
-		defer tx.Rollback()
 
-		_, _, err = d.winningRev(ctx, tx, docID)
-		switch {
-		case errors.Is(err, sql.ErrNoRows):
-			return nil, &internal.Error{Status: http.StatusNotFound, Message: "missing"}
-		case err != nil:
-			return nil, err
+		// Call rows.Next() to see if we get any results at all. If zero results,
+		// we need to return 404 instead of an iterator for the open_revs=all case.
+		if !rows.Next() {
+			if err := rows.Err(); err != nil {
+				return nil, err
+			}
+			return nil, &internal.Error{Message: "missing", Status: http.StatusNotFound}
 		}
-		return nil, tx.Commit()
+
+		return &openRevsRows{
+			id:   docID,
+			ctx:  ctx,
+			pre:  true,
+			rows: rows,
+		}, nil
 	}
 	for _, rev := range revs {
 		if _, err := parseRev(rev); err != nil {
 			return nil, &internal.Error{Message: "invalid rev format", Status: http.StatusBadRequest}
 		}
 	}
-	return nil, nil
+
+	return nil, errors.New("TODO: not implemented")
 }
+
+type openRevsRows struct {
+	id  string
+	ctx context.Context
+	// pre is during instantiation to indicate that the first call to Next has
+	// already been done, so Next() should skip the next call to Next()
+	pre  bool
+	rows *sql.Rows
+}
+
+var _ driver.Rows = (*openRevsRows)(nil)
+
+func (r *openRevsRows) Next(row *driver.Row) error {
+	if !r.pre && !r.rows.Next() {
+		if err := r.rows.Err(); err != nil {
+			return err
+		}
+		return io.EOF
+	}
+	r.pre = false
+	var (
+		deleted bool
+		doc     []byte
+	)
+	if err := r.rows.Scan(&row.Rev, &deleted, &doc); err != nil {
+		return err
+	}
+	toMerge := fullDoc{
+		ID:  r.id,
+		Rev: row.Rev,
+		Doc: doc,
+	}
+	row.ID = r.id
+	row.Doc = toMerge.toReader()
+	return nil
+}
+
+func (r *openRevsRows) Close() error {
+	return r.rows.Close()
+}
+
+func (*openRevsRows) Offset() int64     { return 0 }
+func (*openRevsRows) UpdateSeq() string { return "" }
+func (*openRevsRows) TotalRows() int64  { return 0 }
--- x/sqlite/openrevs_test.go
+++ x/sqlite/openrevs_test.go
@@ -58,23 +58,40 @@ func TestDBOpenRevs(t *testing.T) {
 			wantStatus: http.StatusBadRequest,
 		}
 	})
+	tests.Add("all, with single rev", func(t *testing.T) interface{} {
+		d := newDB(t)
+		docID := "foo"
+		rev := d.tPut(docID, map[string]string{"foo": "bar"})
+
+		return test{
+			db:    d,
+			docID: docID,
+			revs:  []string{"all"},
+			want: []rowResult{
+				{ID: docID, Rev: rev, Doc: `{"_id":"` + docID + `","_rev":"` + rev + `","foo":"bar"}`},
+			},
+		}
+	})
 	/*
 		TODO:
+		- leaf rev is deleted -- returned as usual
 		- No revs provided == returns winning leaf
 		- document not found, open_revs=["something"] = 200 + missing
 		- document found, rev not found
 		- all revs
 		- latest=true
+		- non-leaf rev specified -- missing, I think
+		- Include attachment info when relevant (https://docs.couchdb.org/en/stable/replication/protocol.html#:~:text=In%20case%20the%20Document%20contains%20attachments%2C%20Source%20MUST%20return%20information%20only%20for%20those%20ones%20that%20had%20been%20changed%20(added%20or%20updated)%20since%20the%20specified%20Revision%20values.%20If%20an%20attachment%20was%20deleted%2C%20the%20Document%20MUST%20NOT%20have%20stub%20information%20for%20it)
 
 		Do other GET options have any effect?
-		- attachments
-		- att_encoding_info
-		- atts_since
+		- attachments - No
+		- att_encoding_info - No
+		- atts_since - No
 		- conflicts
 		- deleted_conflicts
 		- local_seq
 		- meta
-		- rev
+		- rev - no
 		- revs
 		- revs_info
 		- If-None-Match header

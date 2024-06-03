bfddb18ccf26f4cb31654a51df8eb5265194a75d
Jonathan Hall <flimzy@flimzy.com>
2024-06-03 14:42:15 +0200

    Remove unncessary IF NOT EXISTS condition from sql schema queries

--- x/sqlite/schema.go
+++ x/sqlite/schema.go
@@ -87,7 +87,7 @@ var schema = []string{
 }
 
 var viewSchema = []string{
-	`CREATE TABLE IF NOT EXISTS {{ .Map }} (
+	`CREATE TABLE {{ .Map }} (
 		pk INTEGER PRIMARY KEY,
 		id TEXT NOT NULL,
 		rev INTEGER NOT NULL,
@@ -96,5 +96,5 @@ var viewSchema = []string{
 		value TEXT,
 		FOREIGN KEY (id, rev, rev_id) REFERENCES {{ .Docs }} (id, rev, rev_id)
 	)`,
-	`CREATE INDEX IF NOT EXISTS {{ .IndexMap }} ON {{ .Map }} (key)`,
+	`CREATE INDEX {{ .IndexMap }} ON {{ .Map }} (key)`,
 }

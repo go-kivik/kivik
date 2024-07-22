4e5dd048a7d4aeb74d1b1e5fecb7f6a9a35bb14c
Jonathan Hall <flimzy@flimzy.com>
2024-07-22 17:08:11 +0200

    service name

--- script/complete_couch2.sh
+++ script/complete_couch2.sh
@@ -3,7 +3,6 @@
 set -x
 
 for db in _users _replicator _global_changes; do
-echo ${1}/${db}
     status=$(curl --silent --write-out "%{http_code}" -o /dev/null -u ${COUCHDB_USER}:${COUCHDB_PASSWORD} -X PUT "${1}/${db}")
     case ${status} in
         2*)

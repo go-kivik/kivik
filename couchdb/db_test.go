054b9932884009ec4dbfb5f6d4fee7de9848e8f4
Jonathan Hall <flimzy@flimzy.com>
2025-03-17 14:40:44 -0400

    wip

--- couchdb/db_test.go
+++ couchdb/db_test.go
@@ -1112,6 +1112,148 @@ func TestPut(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	type test struct {
+		db             *db
+		ddoc, funcName string
+		id             string
+		doc            interface{}
+		options        kivik.Option
+		rev            string
+		status         int
+		err            string
+		finish         func() error
+	}
+
+	tests := testy.NewTable()
+	tests.Add("network error", test{
+		id:     "foo",
+		db:     newTestDB(nil, errors.New("net error")),
+		status: http.StatusBadGateway,
+		err:    `Put "?http://example.com/testdb/foo"?: net error`,
+	})
+	// tests.Add("error response", test{
+	// 	id: "foo",
+	// 	db: newTestDB(&http.Response{
+	// 		StatusCode: http.StatusBadRequest,
+	// 		Body:       io.NopCloser(strings.NewReader("")),
+	// 	}, nil),
+	// 	status: http.StatusBadRequest,
+	// 	err:    "Bad Request",
+	// })
+	// tests.Add("invalid JSON response", test{
+	// 	id: "foo",
+	// 	db: newTestDB(&http.Response{
+	// 		StatusCode: http.StatusOK,
+	// 		Body:       io.NopCloser(strings.NewReader("invalid json")),
+	// 	}, nil),
+	// 	status: http.StatusBadGateway,
+	// 	err:    "invalid character 'i' looking for beginning of value",
+	// })
+	// tests.Add("invalid document", test{
+	// 	id:  "foo",
+	// 	doc: make(chan int),
+	// 	db: newTestDB(&http.Response{
+	// 		StatusCode: http.StatusOK,
+	// 		Body:       io.NopCloser(strings.NewReader("")),
+	// 	}, nil),
+	// 	status: http.StatusBadRequest,
+	// 	err:    `Put "?http://example.com/testdb/foo"?: json: unsupported type: chan int`,
+	// })
+	// tests.Add("doc created, 1.6.1", test{
+	// 	id:  "foo",
+	// 	doc: map[string]string{"foo": "bar"},
+	// 	db: newTestDB(&http.Response{
+	// 		StatusCode: http.StatusCreated,
+	// 		Header: http.Header{
+	// 			"Server":         {"CouchDB/1.6.1 (Erlang OTP/17)"},
+	// 			"Location":       {"http://localhost:5984/foo/foo"},
+	// 			"ETag":           {`"1-4c6114c65e295552ab1019e2b046b10e"`},
+	// 			"Date":           {"Wed, 25 Oct 2017 12:33:09 GMT"},
+	// 			"Content-Type":   {"text/plain; charset=utf-8"},
+	// 			"Content-Length": {"66"},
+	// 			"Cache-Control":  {"must-revalidate"},
+	// 		},
+	// 		Body: io.NopCloser(strings.NewReader(`{"ok":true,"id":"foo","rev":"1-4c6114c65e295552ab1019e2b046b10e"}`)),
+	// 	}, nil),
+	// 	rev: "1-4c6114c65e295552ab1019e2b046b10e",
+	// })
+	// tests.Add("full commit", test{
+	// 	db: newCustomDB(func(req *http.Request) (*http.Response, error) {
+	// 		if err := consume(req.Body); err != nil {
+	// 			return nil, err
+	// 		}
+	// 		if fullCommit := req.Header.Get("X-Couch-Full-Commit"); fullCommit != "true" {
+	// 			return nil, errors.New("X-Couch-Full-Commit not true")
+	// 		}
+	// 		return nil, errors.New("success")
+	// 	}),
+	// 	id:      "foo",
+	// 	doc:     map[string]string{"foo": "bar"},
+	// 	options: OptionFullCommit(),
+	// 	status:  http.StatusBadGateway,
+	// 	err:     `Put "?http://example.com/testdb/foo"?: success`,
+	// })
+	// tests.Add("connection refused", test{
+	// 	db: func() *db {
+	// 		c, err := chttp.New(&http.Client{}, "http://127.0.0.1:1/", mock.NilOption)
+	// 		if err != nil {
+	// 			t.Fatal(err)
+	// 		}
+	// 		return &db{
+	// 			client: &client{Client: c},
+	// 			dbName: "animals",
+	// 		}
+	// 	}(),
+	// 	id:     "cow",
+	// 	doc:    map[string]interface{}{"feet": 4},
+	// 	status: http.StatusBadGateway,
+	// 	err:    `Put "?http://127.0.0.1:1/animals/cow"?: dial tcp ([::1]|127.0.0.1):1: (getsockopt|connect): connection refused`,
+	// })
+	// tests.Add("real database, multipart attachments", func(t *testing.T) interface{} {
+	// 	db := realDB(t)
+
+	// 	return test{
+	// 		name: "real database, multipart attachments",
+	// 		db:   db,
+	// 		id:   "foo",
+	// 		doc: map[string]interface{}{
+	// 			"feet": 4,
+	// 			"_attachments": &kivik.Attachments{
+	// 				"foo.txt": &kivik.Attachment{Filename: "foo.txt", ContentType: "text/plain", Content: Body("test content")},
+	// 			},
+	// 		},
+	// 		rev: "1-1e527110339245a3191b3f6cbea27ab1",
+	// 		finish: func() error {
+	// 			return db.client.DestroyDB(context.Background(), db.dbName, nil)
+	// 		},
+	// 	}
+	// })
+
+	tests.Run(t, func(t *testing.T, tt test) {
+		if tt.finish != nil {
+			t.Cleanup(func() {
+				if err := tt.finish(); err != nil {
+					t.Fatal(err)
+				}
+			})
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		opts := tt.options
+		if opts == nil {
+			opts = mock.NilOption
+		}
+		rev, err := tt.db.Update(ctx, tt.ddoc, tt.funcName, tt.id, tt.doc, opts)
+		if d := internal.StatusErrorDiffRE(tt.err, tt.status, err); d != "" {
+			t.Error(d)
+		}
+		if rev != tt.rev {
+			t.Errorf("Unexpected rev: %s", rev)
+		}
+	})
+}
+
 func TestDelete(t *testing.T) {
 	type tt struct {
 		db      *db

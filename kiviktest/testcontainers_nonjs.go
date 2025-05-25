9bbccde5b0544ca5ea91cd20f55cde67e44255c2
Jonathan Hall <flimzy@flimzy.com>
2025-05-24 21:56:53 -0400

    Move testcontainers core logic to shared lib for use by both Go and GopherJS tests

--- kiviktest/testcontainers_nonjs.go
+++ kiviktest/testcontainers_nonjs.go
@@ -15,71 +15,20 @@
 package kiviktest
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
+	"github.com/go-kivik/kivik/v4/kiviktest/testcontainers/shared"
 )
 
 func startCouchDB(t *testing.T, image string) string { //nolint:thelper // Not a helper
 	if os.Getenv("USETC") == "" {
 		t.Skip("USETC not set, skipping testcontainers")
 	}
-	req := testcontainers.ContainerRequest{
-		Image:        image,
-		ExposedPorts: []string{"5984/tcp"},
-		WaitingFor:   wait.ForHTTP("/").WithPort("5984/tcp").WithStartupTimeout(120 * time.Second),
-		Env: map[string]string{
-			"COUCHDB_USER":     "admin",
-			"COUCHDB_PASSWORD": "abc123",
-		},
-	}
-	container, err := testcontainers.GenericContainer(context.TODO(), testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	ip, err := container.Host(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
-	mappedPort, err := container.MappedPort(context.TODO(), "5984/tcp")
-	if err != nil {
-		t.Fatal(err)
-	}
-	dsn := fmt.Sprintf("http://admin:abc123@%s:%s", ip, mappedPort.Port())
-	for _, db := range []string{"_replicator", "_users", "_global_changes"} {
-		put(t, dsn+"/"+db, nil)
-	}
-	put(t, dsn+"/_node/nonode@nohost/_config/replicator/interval", strings.NewReader(`"1000"`))
-	put(t, dsn+"/_node/nonode@nohost/_config/replicator/worker_processes", strings.NewReader(`"1"`))
-	return dsn
-}
 
-func put(t *testing.T, path string, body io.Reader) {
-	t.Helper()
-	rq, err := http.NewRequest(http.MethodPut, path, body)
+	dsn, err := shared.StartCouchDB(t.Context(), image)
 	if err != nil {
 		t.Fatal(err)
 	}
-	rq.Header.Set("Content-Type", "application/json")
-	resp, err := http.DefaultClient.Do(rq)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	switch resp.StatusCode {
-	case http.StatusOK, http.StatusCreated, http.StatusPreconditionFailed:
-		return
-	}
-	t.Fatalf("Failed to create %s: %s", path, resp.Status)
+	return dsn
 }

6c9e9b4c3292d708f80ec1cc5314615827e4d25d
Jonathan Hall <jonathan@jhall.io>
2026-02-26 13:05:52 -0500

    Add UpdateFunc type and Update() JS compiler for CouchDB update functions

---
+++ x/sqlite/js/js_test.go
@@ -0,0 +1,66 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package js
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"gitlab.com/flimzy/testy"
+)
+
+func TestUpdate(t *testing.T) {
+	t.Parallel()
+
+	type test struct {
+		code       string
+		doc        any
+		req        any
+		wantNewDoc any
+		wantResp   string
+		wantErr    string
+	}
+
+	tests := testy.NewTable()
+	tests.Add("sets updated field and returns OK", test{
+		code:       `function(doc, req) { doc.updated = true; return [doc, "OK"]; }`,
+		doc:        map[string]any{"_id": "foo"},
+		req:        map[string]any{},
+		wantNewDoc: map[string]any{"_id": "foo", "updated": true},
+		wantResp:   "OK",
+	})
+
+	tests.Run(t, func(t *testing.T, tt test) {
+		fn, err := Update(tt.code)
+		if !testy.ErrorMatchesRE(tt.wantErr, err) {
+			t.Fatalf("Update() error = %v, wantErr /%s/", err, tt.wantErr)
+		}
+		if err != nil {
+			return
+		}
+
+		gotNewDoc, gotResp, err := fn(tt.doc, tt.req)
+		if !testy.ErrorMatchesRE(tt.wantErr, err) {
+			t.Errorf("unexpected error: %v, want /%s/", err, tt.wantErr)
+		}
+		if err != nil {
+			return
+		}
+		if d := cmp.Diff(tt.wantResp, gotResp); d != "" {
+			t.Errorf("response mismatch (-want +got):\n%s", d)
+		}
+		if d := cmp.Diff(tt.wantNewDoc, gotNewDoc); d != "" {
+			t.Errorf("newDoc mismatch (-want +got):\n%s", d)
+		}
+	})
+}
